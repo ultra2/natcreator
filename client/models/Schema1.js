cb5cecf9c630e054c684992796daa214fb0d51a5
ultra2 <user10000@freemail.hu>
2012-04-27 09:45:57 -0700

    commit

---
+++ client/models/Schema1.js
@@ -0,0 +1,8 @@
+Ext.define('designer.model.Schema1', { 
+ 	 extend: 'designer.model.MySchema',
+ 	 
+ 	 fields: [
+ 	 	 { name: 'prop2', type: 'string' }
+ 	 ],
+ });
+ 
---
+++ client/models/Schema2.js
@@ -0,0 +1,8 @@
+Ext.define('designer.model.Schema2', { 
+ 	 extend: 'designer.model.MySchema',
+ 	 
+ 	 fields: [
+ 	 	 { name: 'prop3', type: 'string' }
+ 	 ],
+ });
+ 
