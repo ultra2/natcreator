0c5e66b8091ccafd76756e0de6e2f6830794d066
ultra2 <user10000@freemail.hu>
2012-04-27 09:59:10 -0700

    commit

--- client/models/CRBaseObject.js
+++
@@ -1,9 +0,0 @@
-Ext.define('designer.model.CRBaseObject', { 
- 	 extend: 'natjs.data.NATModel',
- 	 
- 	 fields: [
- 	 	 { name: '_id', type: 'string' }
- 	 	 { name: 'name', type: 'string' }
- 	 ],
- });
- 
