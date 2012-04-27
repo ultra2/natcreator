966e943d7eb004f2d8e8137ea4633d90ac04ddd8
ultra2 <user10000@freemail.hu>
2012-04-27 11:28:11 -0700

    commit

---
+++ client/models/CRAccountProject.js
@@ -0,0 +1,10 @@
+Ext.define('designer.model.CRAccountProject', { 
+ 	 extend: 'designer.model.CRBaseObject'
+ 	 
+ 	 ,fields: [
+ 	 	 ,{ name: 'account_id', type: 'string' }
+ 	 	 ,{ name: 'project_id', type: 'string' }
+ 	 	 ,{ name: 'role_id', type: 'string' }
+ 	 ]
+ });
+ 
