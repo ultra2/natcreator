48218ac0ad624c0f46e5b796c53d5c0b0f553bda
ultra2 <user10000@freemail.hu>
2012-04-29 16:43:07 -0700

    commit

---
+++ client/models/CRMetaRow.js
@@ -0,0 +1,15 @@
+Ext.define('designer.model.CRMetaRow', { 
+ 	 extend: 'designer.model.CRBaseObject',
+ 	 alias: 'CRMetaRow'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'parentId', type: 'string' }
+ 	 	 { name: 'depth', type: 'int' }
+ 	 	 { name: 'leaf', type: 'boolean' }
+ 	 	 { name: 'index', type: 'int' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ });
+ Ext.data.NodeInterface.decorate(Ext.create('designer.model.CRMetaRow'));
+ 
