0925cf57419c279ede6a4e333049a3e7d974309c
ultra2 <user10000@freemail.hu>
2012-04-29 14:45:26 -0700

    commit

--- client/models/CRAccountProjectRole.js
+++
@@ -1,9 +0,0 @@
-Ext.define('designer.model.CRAccountProjectRole', { 
- 	 extend: 'designer.model.CRBaseObject'
- 	 
- 	 ,fields: [
- 	 ]
- 	 ,associations: [
- 	 ]
- });
- 
--- client/models/LookupProperty.js
+++
@@ -1,9 +0,0 @@
-Ext.define('designer.model.LookupProperty', { 
- 	 extend: 'designer.model.DataProperty'
- 	 
- 	 ,fields: [
- 	 ]
- 	 ,associations: [
- 	 ]
- });
- 
