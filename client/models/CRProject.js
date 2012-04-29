5c22f083faec91ba576e7dc65dad8be24b034509
ultra2 <user10000@freemail.hu>
2012-04-29 15:51:33 -0700

    commit

---
+++ client/models/CRProject.js
@@ -0,0 +1,11 @@
+Ext.define('designer.model.CRProject', { 
+ 	 extend: 'designer.model.CRBaseObject',
+ 	 alias: 'CRProject'
+ 	 
+ 	 ,fields: [
+ 	 ]
+ 	 ,associations: [
+ 	 	 ,{ type: 'belongsTo', name: 'connection', model: 'designer.model.CRConnection' }
+ 	 ]
+ });
+ 
