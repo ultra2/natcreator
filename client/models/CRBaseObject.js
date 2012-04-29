87edd18ab0ac628f5ab35a8251e84e643385350b
ultra2 <user10000@freemail.hu>
2012-04-29 16:33:04 -0700

    commit

---
+++ client/models/CRBaseObject.js
@@ -0,0 +1,19 @@
+Ext.define('designer.model.CRBaseObject', { 
+ 	 extend: 'natjs.data.NATModel',
+ 	 alias: 'CRBaseObject'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: '_id', type: 'string', debug: true }
+ ,	 	 { name: 'name', type: 'string', defaultValue: 'new' }
+ ,	 	 { name: 'displayName', type: 'string' }
+ ,	 	 { name: 'remark', type: 'string' }
+ ,	 	 { name: 'isSystem', type: 'boolean', debug: true }
+ ,	 	 { name: 'created', type: 'date', defaultValue: new Date(0) }
+ ,	 	 { name: 'modified', type: 'date', defaultValue: new Date(0), debug: true }
+ ,	 	 { name: 'deleted', type: 'boolean', debug: true }
+ ,	 	 { name: 'version', type: 'int', debug: true }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ });
+ 
---
+++ client/models/CRConnection.js
@@ -0,0 +1,15 @@
+Ext.define('designer.model.CRConnection', { 
+ 	 extend: 'designer.model.CRBaseObject',
+ 	 alias: 'CRConnection'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'username', type: 'string' }
+ ,	 	 { name: 'password', type: 'string' }
+ ,	 	 { name: 'database', type: 'string' }
+ ,	 	 { name: 'server', type: 'string' }
+ ,	 	 { name: 'port', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ });
+ 
