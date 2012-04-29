aefc4eb91e1ad99a81b063cdf477dc5787458706
ultra2 <user10000@freemail.hu>
2012-04-29 16:30:14 -0700

    commit

---
+++ client/models/SchemaFile.js
@@ -0,0 +1,11 @@
+Ext.define('designer.model.SchemaFile', { 
+ 	 extend: 'designer.model.File',
+ 	 alias: 'SchemaFile'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'source', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ });
+ 
