9826f5e3a12a35ed832706c6115a5e5f3dda1ac9
ultra2 <user10000@freemail.hu>
2012-04-29 16:14:27 -0700

    commit

---
+++ client/models/ObjectProperty.js
@@ -0,0 +1,11 @@
+Ext.define('designer.model.ObjectProperty', { 
+ 	 extend: 'designer.model.DataProperty',
+ 	 alias: 'ObjectProperty'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'type_id', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ });
+ 
