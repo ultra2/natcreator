e6bcae982a19f6d9f6ebd02188ec20c41e0254fe
ultra2 <user10000@freemail.hu>
2012-04-29 16:10:14 -0700

    commit

---
+++ client/models/CRAccountProjectRole.js
@@ -0,0 +1,10 @@
+Ext.define('designer.model.CRAccountProjectRole', { 
+ 	 extend: 'designer.model.CRBaseObject',
+ 	 alias: 'CRAccountProjectRole'
+ 	 
+ 	 ,fields: [
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ });
+ 
---
+++ client/models/DataProperty.js
@@ -0,0 +1,14 @@
+Ext.define('designer.model.DataProperty', { 
+ 	 extend: 'designer.model.CRBaseObject',
+ 	 alias: 'DataProperty'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'index', type: 'int' }
+ 	 	 ,	 	 { name: 'isKey', type: 'boolean' }
+ 	 	 ,	 	 { name: 'isDebug', type: 'boolean' }
+ 	 	 ,	 	 { name: 'isNullable', type: 'boolean' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ });
+ 
