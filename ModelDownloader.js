f6b62e9028a516ec5919d593cda01c2476125af0
ultra2 <user10000@freemail.hu>
2012-04-30 16:22:58 -0700

    commit

--- ModelDownloader.js
+++ ModelDownloader.js
@@ -1,2 +1,25 @@
-downloader
+Ext.syncRequire('CRBaseObject');
+ Ext.syncRequire('CRAccount');
+ Ext.syncRequire('CRConnection');
+ Ext.syncRequire('CRProject');
+ Ext.syncRequire('CRAccountProject');
+ Ext.syncRequire('CRAccountProjectRole');
+ Ext.syncRequire('Generator');
+ Ext.syncRequire('SchemaGenerator');
+ Ext.syncRequire('SchemaListGenerator');
+ Ext.syncRequire('GenerationStep');
+ Ext.syncRequire('File');
+ Ext.syncRequire('DataProperty');
+ Ext.syncRequire('Schema');
+ Ext.syncRequire('ProjectSetting');
+ Ext.syncRequire('StringProperty');
+ Ext.syncRequire('BooleanProperty');
+ Ext.syncRequire('DateProperty');
+ Ext.syncRequire('IntProperty');
+ Ext.syncRequire('ReferenceProperty');
+ Ext.syncRequire('ObjectProperty');
+ Ext.syncRequire('AutoProperty');
+ Ext.syncRequire('CRMetaRow');
+ Ext.syncRequire('CRMetaCategoryRow');
+ Ext.syncRequire('CRMetaPropertyRow');
  
