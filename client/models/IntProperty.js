4ec3ccb9c020843b425e46813446d56a6b391e83
ultra2 <user10000@freemail.hu>
2012-04-30 06:40:46 -0700

    commit

---
+++ client/models/IntProperty.js
@@ -0,0 +1,11 @@
+Ext.define('designer.model.IntProperty', { 
+ 	 extend: 'designer.model.DataProperty',
+ 	 alias: 'IntProperty'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'defaultValue', type: 'int' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ });
+ 
