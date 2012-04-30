4dc63473ab8861e6ac7f8d22f8e879b21fde6bf2
ultra2 <user10000@freemail.hu>
2012-04-30 09:29:52 -0700

    commit

---
+++ client/models/ReferenceProperty.js
@@ -0,0 +1,11 @@
+Ext.define('designer.model.ReferenceProperty', { 
+ 	 extend: 'designer.model.DataProperty',
+ 	 alias: 'ReferenceProperty'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'type_id', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ });
+ 
