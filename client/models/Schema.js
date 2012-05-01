62f66b56406138f1adfa9b7e4d0ae33b54e6329f
ultra2 <user10000@freemail.hu>
2012-04-30 18:45:27 -0700

    commit

--- client/models/Schema.js
+++ client/models/Schema.js
@@ -4,10 +4,10 @@ Ext.define('designer.model.Schema', {
  	 
  	 ,fields: [
  	 	 { name: 'isHierarchy', type: 'boolean' }
- 	 	 ,{ name: 'parentId', type: 'string' }
- 	 	 ,{ name: 'leaf', type: 'boolean' }
- 	 	 ,{ name: 'depth', type: 'int' }
- 	 	 ,{ name: 'index', type: 'int' }
+ 	 	 ,{ name: 'parentId', type: 'string', debug: true }
+ 	 	 ,{ name: 'leaf', type: 'boolean', debug: true }
+ 	 	 ,{ name: 'depth', type: 'int', debug: true }
+ 	 	 ,{ name: 'index', type: 'int', debug: true }
  	 ]
  	 ,associations: [
  	 	 { type: 'hasMany', name: 'properties', associationKey: 'properties', model: 'designer.model.DataProperty' , primaryKey: '_id' }
