1cc9c13a92dbe6d329e9964a4d931d0328d867e7
ultra2 <user10000@freemail.hu>
2012-05-02 16:52:41 -0700

    commit

---
+++ client/models/Login.js
@@ -0,0 +1,12 @@
+Ext.define('designer.model.Login', { 
+ 	 extend: 'designer.model.CRBaseObject',
+ 	 alias: 'Login'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'username', type: 'string' }
+ 	 	 ,{ name: 'password', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ });
+ 
