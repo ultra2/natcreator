37787773807194133e2f3e3ee0287a4edd82f522
ultra2 <user10000@freemail.hu>
2012-05-09 14:10:18 -0700

    commit

---
+++ client/models/SchemaListGenerator.js
@@ -0,0 +1,14 @@
+Ext.define('designer.model.SchemaListGenerator', { 
+ 	 extend: 'designer.model.Generator',
+ 	 alias: 'SchemaListGenerator'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'source', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 	 { name: 'source' }
+ 	 ]
+ });
+ 
