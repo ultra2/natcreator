0e75b14b19c21f5d90a5e3c6657f87fe679df1dd
ultra2 <user10000@freemail.hu>
2012-05-09 02:09:45 -0700

    commit

--- ModelDownloader.js
+++ ModelDownloader.js
@@ -1,12 +1,13 @@
 Ext.define('designer.ModelDownloader', { 
  	 Download: function() {
  	 	 Ext.syncRequire('designer.model.CRBaseObject');
- 	 	 Ext.syncRequire('designer.model.communication');
- 	 	 Ext.syncRequire('designer.model.Login');
- 	 	 Ext.syncRequire('designer.model.registration');
- 	 	 Ext.syncRequire('designer.model.persistent');
+ 	 	 Ext.syncRequire('designer.model.persistentM');
  	 	 Ext.syncRequire('designer.model.CRAccountProjectRole');
- 	 	 Ext.syncRequire('designer.model.entity');
+ 	 	 Ext.syncRequire('designer.model.CRAccountProject');
+ 	 	 Ext.syncRequire('designer.model.CRConnection');
+ 	 	 Ext.syncRequire('designer.model.CRProject');
+ 	 	 Ext.syncRequire('designer.model.CRAccount');
+ 	 	 Ext.syncRequire('designer.model.persistentC');
  	 	 Ext.syncRequire('designer.model.DataProperty');
  	 	 Ext.syncRequire('designer.model.Schema');
  	 	 Ext.syncRequire('designer.model.StringProperty');
@@ -16,20 +17,19 @@ Ext.define('designer.ModelDownloader', {
  	 	 Ext.syncRequire('designer.model.ReferenceProperty');
  	 	 Ext.syncRequire('designer.model.ObjectProperty');
  	 	 Ext.syncRequire('designer.model.AutoProperty');
- 	 	 Ext.syncRequire('designer.model.CRAccount');
- 	 	 Ext.syncRequire('designer.model.CRConnection');
- 	 	 Ext.syncRequire('designer.model.CRProject');
  	 	 Ext.syncRequire('designer.model.File');
  	 	 Ext.syncRequire('designer.model.GenerationStep');
  	 	 Ext.syncRequire('designer.model.ProjectSetting');
  	 	 Ext.syncRequire('designer.model.Generator');
  	 	 Ext.syncRequire('designer.model.SchemaGenerator');
  	 	 Ext.syncRequire('designer.model.SchemaListGenerator');
- 	 	 Ext.syncRequire('designer.model.CRAccountProject');
  	 	 Ext.syncRequire('designer.model.client');
  	 	 Ext.syncRequire('designer.model.CRMetaRow');
  	 	 Ext.syncRequire('designer.model.CRMetaCategoryRow');
  	 	 Ext.syncRequire('designer.model.CRMetaPropertyRow');
+ 	 	 Ext.syncRequire('designer.model.communication');
+ 	 	 Ext.syncRequire('designer.model.Login');
+ 	 	 Ext.syncRequire('designer.model.registration');
  	 }
  });
  
