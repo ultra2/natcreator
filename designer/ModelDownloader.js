686cd371151fa16da033b220fc0fe98a53e6a388
ultra2 <user10000@freemail.hu>
2012-05-14 15:02:47 -0700

    commit

---
+++ designer/ModelDownloader.js
@@ -0,0 +1,36 @@
+Ext.define('designer.ModelDownloader', { 
+ 	 Download: function() {
+ 	 	 Ext.syncRequire('designer.model.CRBaseObject');
+ 	 	 Ext.syncRequire('designer.model.persistentM');
+ 	 	 Ext.syncRequire('designer.model.CRAccountProjectRole');
+ 	 	 Ext.syncRequire('designer.model.CRAccountProject');
+ 	 	 Ext.syncRequire('designer.model.CRConnection');
+ 	 	 Ext.syncRequire('designer.model.CRProject');
+ 	 	 Ext.syncRequire('designer.model.CRAccount');
+ 	 	 Ext.syncRequire('designer.model.persistentC');
+ 	 	 Ext.syncRequire('designer.model.DataProperty');
+ 	 	 Ext.syncRequire('designer.model.Schema');
+ 	 	 Ext.syncRequire('designer.model.StringProperty');
+ 	 	 Ext.syncRequire('designer.model.BooleanProperty');
+ 	 	 Ext.syncRequire('designer.model.DateProperty');
+ 	 	 Ext.syncRequire('designer.model.IntProperty');
+ 	 	 Ext.syncRequire('designer.model.ReferenceProperty');
+ 	 	 Ext.syncRequire('designer.model.ObjectProperty');
+ 	 	 Ext.syncRequire('designer.model.AutoProperty');
+ 	 	 Ext.syncRequire('designer.model.GenerationStep');
+ 	 	 Ext.syncRequire('designer.model.ProjectSetting');
+ 	 	 Ext.syncRequire('designer.model.Generator');
+ 	 	 Ext.syncRequire('designer.model.SchemaGenerator');
+ 	 	 Ext.syncRequire('designer.model.SchemaListGenerator');
+ 	 	 Ext.syncRequire('designer.model.client');
+ 	 	 Ext.syncRequire('designer.model.CRMetaRow');
+ 	 	 Ext.syncRequire('designer.model.CRMetaCategoryRow');
+ 	 	 Ext.syncRequire('designer.model.CRMetaPropertyRow');
+ 	 	 Ext.syncRequire('designer.model.communication');
+ 	 	 Ext.syncRequire('designer.model.Login');
+ 	 	 Ext.syncRequire('designer.model.registration');
+ 	 	 Ext.syncRequire('designer.model.File');
+ 	 	 Ext.syncRequire('designer.model.AccountInfo');
+ 	 }
+ });
+ 
