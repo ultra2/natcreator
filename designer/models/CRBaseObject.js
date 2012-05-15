a86fce554879c4d911d7b2bbd88c7c6c964990d8
ultra2 <user10000@freemail.hu>
2012-05-15 09:15:30 -0700

    commit

---
+++ designer/models/CRBaseObject.js
@@ -0,0 +1,16 @@
+Ext.define('designer.model.CRBaseObject', { 
+ 	 extend: 'natjs.data.NATModel',
+ 	 alias: 'CRBaseObject'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: '_id', type: 'string', debug: true }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 	 { field: '_id', type: 'presence' }
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
---
+++ designer/models/Schema.js
@@ -0,0 +1,31 @@
+Ext.define('designer.model.Schema', { 
+ 	 extend: 'designer.model.persistentC',
+ 	 alias: 'Schema'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'isHierarchy', type: 'boolean' }
+ 	 	 ,{ name: 'parentId', type: 'string', debug: true }
+ 	 	 ,{ name: 'leaf', type: 'boolean', debug: true }
+ 	 	 ,{ name: 'depth', type: 'int', debug: true }
+ 	 	 ,{ name: 'index', type: 'int', debug: true }
+ 	 	 ,{ name: 'name', type: 'string' }
+ 	 	 ,{ name: 'strParam1', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 	 { type: 'hasMany', name: 'properties', associationKey: 'properties', model: 'designer.model.DataProperty' , primaryKey: '_id' }
+ 	 ]
+ 	 ,validations: [
+ 	 	 { field: 'isHierarchy', type: 'presence' }
+ 	 	 ,{ field: 'parentId', type: 'presence' }
+ 	 	 ,{ field: 'leaf', type: 'presence' }
+ 	 	 ,{ field: 'depth', type: 'presence' }
+ 	 	 ,{ field: 'index', type: 'presence' }
+ 	 	 ,{ field: 'properties', type: 'presence' }
+ 	 	 ,{ field: 'name', type: 'presence' }
+ 	 	 ,{ field: 'strParam1', type: 'presence' }
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ Ext.data.NodeInterface.decorate(Ext.create('designer.model.Schema'));
+ 
