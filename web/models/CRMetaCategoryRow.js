aff24483379a55ca179941a012ac85584d97a084
ultra2 <user10000@freemail.hu>
2012-05-15 03:39:47 -0700

    commit

---
+++ web/models/CRMetaCategoryRow.js
@@ -0,0 +1,13 @@
+Ext.define('web.model.CRMetaCategoryRow', { 
+ 	 extend: 'web.model.CRMetaRow',
+ 	 alias: 'CRMetaCategoryRow'
+ 	 
+ 	 ,fields: [
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ Ext.data.NodeInterface.decorate(Ext.create('web.model.CRMetaCategoryRow'));
+ 
---
+++ web/models/registration.js
@@ -0,0 +1,14 @@
+Ext.define('web.model.registration', { 
+ 	 extend: 'web.model.communication',
+ 	 alias: 'registration'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'username', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 	 { name: 'username' }
+ 	 ]
+ });
+ 
