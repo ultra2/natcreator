423419a95e01a1f17d7631908a630f9639223a04
ultra2 <user10000@freemail.hu>
2012-05-15 13:57:01 -0700

    commit

---
+++ web/models/StringProperty.js
@@ -0,0 +1,17 @@
+Ext.define('web.model.StringProperty', { 
+ 	 extend: 'web.model.DataProperty',
+ 	 alias: 'StringProperty'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'defaultValue', type: 'string' }
+ 	 	 ,{ name: 'minLength', type: 'int' }
+ 	 	 ,{ name: 'maxLength', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
