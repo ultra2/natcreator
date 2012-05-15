ada5a7faccef9d624a142731f8bd15e8ddbe4a9d
ultra2 <user10000@freemail.hu>
2012-05-15 15:49:23 -0700

    commit

---
+++ web/models/Registration.js
@@ -0,0 +1,23 @@
+Ext.define('web.model.Registration', { 
+ 	 extend: 'web.model.Communication',
+ 	 alias: 'Registration'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'username', type: 'string' }
+ 	 	 ,{ name: 'email', type: 'string' }
+ 	 	 ,{ name: 'password', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 	 { field: 'username', type: 'presence' }
+ 	 	 ,{ field: 'username', type: 'length', min: 6, max: 20 }
+ 	 	 ,{ field: 'email', type: 'presence' }
+ 	 	 ,{ field: 'email', type: 'length', min: 3, max: 0 }
+ 	 	 ,{ field: 'password', type: 'presence' }
+ 	 	 ,{ field: 'password', type: 'length', min: 6, max: 20 }
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
