88fe779ad2fcee39d0ee640976280b72e3385921
ultra2 <user10000@freemail.hu>
2012-05-15 11:15:52 -0700

    commit

--- web/ModelDownloader.js
+++ web/ModelDownloader.js
@@ -1,9 +1,9 @@
 Ext.define('web.ModelDownloader', { 
  	 Download: function() {
  	 	 Ext.syncRequire('web.model.CRBaseObject');
- 	 	 Ext.syncRequire('web.model.communication');
+ 	 	 Ext.syncRequire('web.model.Communication');
  	 	 Ext.syncRequire('web.model.Login');
- 	 	 Ext.syncRequire('web.model.registration');
+ 	 	 Ext.syncRequire('web.model.Registration');
  	 }
  });
  
