a85d5cecfc0f24c4db5fa71c81be34b896e885c2
ultra2 <user10000@freemail.hu>
2012-05-15 10:01:52 -0700

    commit

---
+++ web/models/DataProperty.js
@@ -0,0 +1,26 @@
+Ext.define('web.model.DataProperty', { 
+ 	 extend: 'web.model.persistentC',
+ 	 alias: 'DataProperty'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'index', type: 'int' }
+ 	 	 ,{ name: 'isKey', type: 'boolean' }
+ 	 	 ,{ name: 'isDebug', type: 'boolean' }
+ 	 	 ,{ name: 'isNullable', type: 'boolean' }
+ 	 	 ,{ name: 'isArray', type: 'boolean' }
+ 	 	 ,{ name: 'name', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 	 { field: 'index', type: 'presence' }
+ 	 	 ,{ field: 'isKey', type: 'presence' }
+ 	 	 ,{ field: 'isDebug', type: 'presence' }
+ 	 	 ,{ field: 'isNullable', type: 'presence' }
+ 	 	 ,{ field: 'isArray', type: 'presence' }
+ 	 	 ,{ field: 'name', type: 'presence' }
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
