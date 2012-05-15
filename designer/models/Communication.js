1dd5901bf1929e49d0c5a19fee5db391533839c9
ultra2 <user10000@freemail.hu>
2012-05-15 14:25:38 -0700

    commit

---
+++ designer/models/Communication.js
@@ -0,0 +1,14 @@
+Ext.define('designer.model.Communication', { 
+ 	 extend: 'designer.model.CRBaseObject',
+ 	 alias: 'Communication'
+ 	 
+ 	 ,fields: [
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
