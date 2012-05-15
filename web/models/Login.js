34b9c43f7e57e259d5d0caafaec377aa99ac2ac3
ultra2 <user10000@freemail.hu>
2012-05-15 10:15:06 -0700

    commit

---
+++ web/models/Login.js
@@ -0,0 +1,18 @@
+Ext.define('web.model.Login', { 
+ 	 extend: 'web.model.communication',
+ 	 alias: 'Login'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'username', type: 'string' }
+ 	 	 ,{ name: 'password', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 	 { field: 'username', type: 'presence' }
+ 	 	 ,{ field: 'password', type: 'presence' }
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
