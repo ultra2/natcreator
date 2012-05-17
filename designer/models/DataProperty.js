cfcde4d58fe76eadd373356d351f5005ee559aa6
ultra2 <user10000@freemail.hu>
2012-05-17 08:45:17 -0700

    commit

---
+++ designer/models/DataProperty.js
@@ -0,0 +1,21 @@
+Ext.define('designer.model.DataProperty', { 
+ 	 extend: 'designer.model.PersistentC',
+ 	 alias: 'DataProperty'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'index', type: 'int' }
+ 	 	 ,{ name: 'isKey', type: 'boolean' }
+ 	 	 ,{ name: 'isDebug', type: 'boolean' }
+ 	 	 ,{ name: 'isNullable', type: 'boolean' }
+ 	 	 ,{ name: 'isArray', type: 'boolean' }
+ 	 	 ,{ name: 'name', type: 'string' }
+ 	 	 ,{ name: 'isPersistent', type: 'boolean', defaultValue: 'true' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
