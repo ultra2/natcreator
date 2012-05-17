15c7ac3121486c08f7ddfa44bffd6031beb8f82e
ultra2 <user10000@freemail.hu>
2012-05-17 09:22:07 -0700

    commit

---
+++ web/models/CRAccount.js
@@ -0,0 +1,26 @@
+Ext.define('web.model.CRAccount', { 
+ 	 extend: 'web.model.PersistentM',
+ 	 alias: 'CRAccount'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'username', type: 'string' }
+ 	 	 ,{ name: 'passwordSalt', type: 'string' }
+ 	 	 ,{ name: 'passwordHash', type: 'string' }
+ 	 	 ,{ name: 'email', type: 'string' }
+ 	 	 ,{ name: 'isActivated', type: 'boolean' }
+ 	 	 ,{ name: 'githubToken', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 	 { field: 'username', type: 'presence' }
+ 	 	 ,{ field: 'passwordSalt', type: 'presence' }
+ 	 	 ,{ field: 'passwordHash', type: 'presence' }
+ 	 	 ,{ field: 'email', type: 'presence' }
+ 	 	 ,{ field: 'isActivated', type: 'presence' }
+ 	 	 ,{ field: 'githubToken', type: 'presence' }
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
