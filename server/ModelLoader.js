0302ad70c48471d855373832713d3b92cd87a5e7
ultra2 <user10000@freemail.hu>
2012-05-24 12:50:06 -0700

    commit

---
+++ server/ModelLoader.js
@@ -0,0 +1,36 @@
+Ext.define('undefinedModelDownloader', { 
+ 	 Download: function() {
+ 	 	 Ext.syncRequire('undefinedmodel.CRBaseObject');
+ 	 	 Ext.syncRequire('undefinedmodel.PersistentM');
+ 	 	 Ext.syncRequire('undefinedmodel.CRAccountProjectRole');
+ 	 	 Ext.syncRequire('undefinedmodel.CRAccountProject');
+ 	 	 Ext.syncRequire('undefinedmodel.CRConnection');
+ 	 	 Ext.syncRequire('undefinedmodel.CRProject');
+ 	 	 Ext.syncRequire('undefinedmodel.CRAccount');
+ 	 	 Ext.syncRequire('undefinedmodel.PersistentC');
+ 	 	 Ext.syncRequire('undefinedmodel.DataProperty');
+ 	 	 Ext.syncRequire('undefinedmodel.Schema');
+ 	 	 Ext.syncRequire('undefinedmodel.StringProperty');
+ 	 	 Ext.syncRequire('undefinedmodel.BooleanProperty');
+ 	 	 Ext.syncRequire('undefinedmodel.DateProperty');
+ 	 	 Ext.syncRequire('undefinedmodel.IntProperty');
+ 	 	 Ext.syncRequire('undefinedmodel.ReferenceProperty');
+ 	 	 Ext.syncRequire('undefinedmodel.ObjectProperty');
+ 	 	 Ext.syncRequire('undefinedmodel.AutoProperty');
+ 	 	 Ext.syncRequire('undefinedmodel.GenerationStep');
+ 	 	 Ext.syncRequire('undefinedmodel.ProjectSetting');
+ 	 	 Ext.syncRequire('undefinedmodel.Generator');
+ 	 	 Ext.syncRequire('undefinedmodel.SchemaGenerator');
+ 	 	 Ext.syncRequire('undefinedmodel.SchemaListGenerator');
+ 	 	 Ext.syncRequire('undefinedmodel.Client');
+ 	 	 Ext.syncRequire('undefinedmodel.CRMetaRow');
+ 	 	 Ext.syncRequire('undefinedmodel.CRMetaCategoryRow');
+ 	 	 Ext.syncRequire('undefinedmodel.CRMetaPropertyRow');
+ 	 	 Ext.syncRequire('undefinedmodel.Communication');
+ 	 	 Ext.syncRequire('undefinedmodel.Login');
+ 	 	 Ext.syncRequire('undefinedmodel.Registration');
+ 	 	 Ext.syncRequire('undefinedmodel.File');
+ 	 	 Ext.syncRequire('undefinedmodel.AccountInfo');
+ 	 }
+ });
+ 
