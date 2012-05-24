17dfa3476ba501137f74a814fb075f616e2caf25
ultra2 <user10000@freemail.hu>
2012-05-24 12:41:50 -0700

    commit

--- server/models/CRMetaCategoryRow.js
+++
@@ -1,9 +0,0 @@
-var CRMetaCategoryRowSchema = new Schema({ 
- 	 	 _id: { type: String }
- 	 	 ,parentId: { type: String }
- 	 	 ,depth: { type: Number }
- 	 	 ,leaf: { type: Boolean }
- 	 	 ,index: { type: Number }
- });
- mongoose.model('CRMetaCategoryRow', CRMetaCategoryRowSchema);
- 
--- server/models/File.js
+++
@@ -1,7 +0,0 @@
-var FileSchema = new Schema({ 
- 	 	 _id: { type: String }
- 	 	 ,content: { type: String }
- 	 	 ,name: { type: String }
- });
- mongoose.model('File', FileSchema);
- 
