11022d6a37e0a3734db80a89bd7e9689bb095920
ultra2 <user10000@freemail.hu>
2012-05-26 11:35:07 -0700

    commit

--- server/schemas/CRMetaCategoryRow.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-exports.CRMetaCategoryRowSchema = new mongoose.Schema({ 
-		_id: { type: String }
-		,parentId: { type: String }
-		,depth: { type: Number }
-		,leaf: { type: Boolean }
-		,index: { type: Number }
-});
