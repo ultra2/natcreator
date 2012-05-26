d0c8d217753a37bc9aae26dfe60287750bffb507
ultra2 <user10000@freemail.hu>
2012-05-26 10:35:08 -0700

    commit

---
+++ server/models/CRMetaCategoryRow.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+ 
+ exports.CRMetaCategoryRowSchema = new mongoose.Schema({ 
+ 	 	 _id: { type: String }
+ 	 	 ,parentId: { type: String }
+ 	 	 ,depth: { type: Number }
+ 	 	 ,leaf: { type: Boolean }
+ 	 	 ,index: { type: Number }
+ });
+ 
---
+++ server/models/CRProject.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+ var CRConnectionSchema = require('../model/CRConnection.js').CRConnectionSchema;
+ 
+ exports.CRProjectSchema = new mongoose.Schema({ 
+ 	 	 _id: { type: String }
+ 	 	 ,created: { type: Date, default: 'Fri Apr 27 2012 21:45:43 GMT+0200 (Central Europe Daylight Time)' }
+ 	 	 ,modified: { type: Date, default: 'Fri Apr 27 2012 21:45:43 GMT+0200 (Central Europe Daylight Time)' }
+ 	 	 ,version: { type: Number }
+ 	 	 ,deleted: { type: Boolean }
+ 	 	 ,name: { type: String }
+ });
+ 
