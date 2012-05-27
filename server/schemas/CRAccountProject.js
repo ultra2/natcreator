a89de75cf3b24601944f0e9b8a6c36902f0bfa58
ultra2 <user10000@freemail.hu>
2012-05-27 08:27:34 -0700

    commit

---
+++ server/schemas/CRAccountProject.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+exports.CRAccountProjectSchema = new mongoose.Schema({ 
+		_id: { type: String }
+		,created: { type: Date, default: 'Fri Apr 27 2012 21:45:43 GMT+0200 (Central Europe Daylight Time)' }
+		,modified: { type: Date, default: 'Fri Apr 27 2012 21:45:43 GMT+0200 (Central Europe Daylight Time)' }
+		,version: { type: Number }
+		,deleted: { type: Boolean }
+		,account_id: { type: String, ref: 'CRAccount' }
+		,project_id: { type: String, ref: 'CRProject' }
+		,role_id: { type: String, ref: 'CRAccountProjectRole' }
+});
