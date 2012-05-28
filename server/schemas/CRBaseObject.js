2a46341574a23a9cecbaef7d033608e276f57a43
ultra2 <user10000@freemail.hu>
2012-05-28 16:51:04 -0700

    commit

--- server/schemas/CRBaseObject.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require('mongoose');
-
-exports.CRBaseObjectSchema = new mongoose.Schema({ 
-		_id: { type: String }
-});
--- server/schemas/Communication.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require('mongoose');
-
-exports.CommunicationSchema = new mongoose.Schema({ 
-		_id: { type: String }
-});
