8d0e945d74f4673260832a8bf7db84b776dcb4c1
ultra2 <user10000@freemail.hu>
2012-05-28 16:52:23 -0700

    commit

--- server/schemas/File.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-
-exports.File = new mongoose.Schema({ 
-		_id: { type: String }
-		,content: { type: String }
-		,name: { type: String }
-});
--- server/schemas/Registration.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-exports.Registration = new mongoose.Schema({ 
-		_id: { type: String }
-		,username: { type: String }
-		,email: { type: String }
-		,password: { type: String }
-		,confirmPassword: { type: String }
-});
