137b02e6e7e237c743ee932b568f6d629df4c5e9
ultra2 <user10000@freemail.hu>
2012-05-28 19:01:03 -0700

    commit

--- server/schemas/CRBaseObject.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var commonPlugin = require('../schemaPlugins/common.js');
-
-exports.CRBaseObject = new mongoose.Schema({ 
-		_id: { type: String }
-});
-
-exports.DataProperty.plugin(commonPlugin);
