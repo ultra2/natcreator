fac268e085855d848e0406a096bbd1312a29c619
ultra2 <user10000@freemail.hu>
2012-05-28 18:50:40 -0700

    commit

---
+++ server/schemas/CRAccountProjectRole.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var commonPlugin = require('../schemaPlugins/common.js');
+
+exports.CRAccountProjectRole = new mongoose.Schema({ 
+		_id: { type: String }
+		,created: { type: Date, default: 'Fri Apr 27 2012 21:45:43 GMT+0200 (Central Europe Daylight Time)' }
+		,modified: { type: Date, default: 'Fri Apr 27 2012 21:45:43 GMT+0200 (Central Europe Daylight Time)' }
+		,version: { type: Number }
+		,deleted: { type: Boolean }
+		,name: { type: String }
+});
+
+exports.DataProperty.plugin(commonPlugin);
---
+++ server/schemas/DateProperty.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var commonPlugin = require('../schemaPlugins/common.js');
+
+exports.DateProperty = new mongoose.Schema({ 
+		_id: { type: String }
+		,created: { type: Date, default: 'Fri Apr 27 2012 21:45:43 GMT+0200 (Central Europe Daylight Time)' }
+		,modified: { type: Date, default: 'Fri Apr 27 2012 21:45:43 GMT+0200 (Central Europe Daylight Time)' }
+		,version: { type: Number }
+		,deleted: { type: Boolean }
+		,index: { type: Number }
+		,isKey: { type: Boolean }
+		,isDebug: { type: Boolean }
+		,isNullable: { type: Boolean }
+		,isArray: { type: Boolean }
+		,name: { type: String }
+		,isPersistent: { type: Boolean, default: true }
+		,defaultValue: { type: Date, default: 'Fri Apr 27 2012 00:00:00 GMT+0200 (Central Europe Daylight Time)' }
+});
+
+exports.DataProperty.plugin(commonPlugin);
