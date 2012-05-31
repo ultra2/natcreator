95888fb4dc9288df59f6603218254181afc3442b
ultra2 <user10000@freemail.hu>
2012-05-30 21:43:07 -0700

    commit

---
+++ server/schemas/CRRegistration.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var commonPlugin = require('../schemaPlugins/common.js');
+
+exports.CRRegistration = new mongoose.Schema({ 
+		_id: { type: String }
+		,created: { type: Date, default: 'Fri Apr 27 2012 21:45:43 GMT+0200 (Central Europe Daylight Time)' }
+		,modified: { type: Date, default: 'Fri Apr 27 2012 21:45:43 GMT+0200 (Central Europe Daylight Time)' }
+		,version: { type: Number }
+		,deleted: { type: Boolean }
+		,username: { type: String }
+		,passwordSalt: { type: String }
+		,passwordHash: { type: String }
+		,email: { type: String }
+});
+
+exports.CRRegistration.plugin(commonPlugin);
