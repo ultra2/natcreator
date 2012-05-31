76b9b38d6443ebc683f78b48d0ae6e80b516e7d3
ultra2 <user10000@freemail.hu>
2012-05-31 14:39:44 -0700

    commit

--- server/schemas/Logout.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var commonPlugin = require('../schemaPlugins/common.js');
-
-exports.Logout = new mongoose.Schema({ 
-		_id: { type: String }
-});
-
-exports.Logout.plugin(commonPlugin);
