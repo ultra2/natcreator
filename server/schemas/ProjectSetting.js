8d0ff2a290ade51ec9e51ff52f4e2a8cc2b57904
ultra2 <user10000@freemail.hu>
2012-05-30 21:51:24 -0700

    commit

---
+++ server/schemas/ProjectSetting.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var commonPlugin = require('../schemaPlugins/common.js');
+var GenerationStep = require('../schemas/GenerationStep.js').GenerationStep;
+
+exports.ProjectSetting = new mongoose.Schema({ 
+		_id: { type: String }
+		,created: { type: Date, default: 'Fri Apr 27 2012 21:45:43 GMT+0200 (Central Europe Daylight Time)' }
+		,modified: { type: Date, default: 'Fri Apr 27 2012 21:45:43 GMT+0200 (Central Europe Daylight Time)' }
+		,version: { type: Number }
+		,deleted: { type: Boolean }
+		,repositoryName: { type: String }
+		,repositoryHost: { type: String }
+		,fontSize: { type: Number }
+		,generationSteps: [GenerationStep]
+});
+
+exports.ProjectSetting.plugin(commonPlugin);
