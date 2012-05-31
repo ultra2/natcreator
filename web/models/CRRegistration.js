59ed8c7bd86b4b145641cfac029569e7fe20481d
ultra2 <user10000@freemail.hu>
2012-05-31 13:49:51 -0700

    commit

---
+++ web/models/CRRegistration.js
@@ -0,0 +1,22 @@
+Ext.define('web.model.CRRegistration', { 
+ 	 extend: 'web.model.PersistentM',
+ 	 alias: 'CRRegistration'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'username', type: 'string' }
+ 	 	 ,{ name: 'passwordSalt', type: 'string' }
+ 	 	 ,{ name: 'passwordHash', type: 'string' }
+ 	 	 ,{ name: 'email', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 	 { field: 'username', type: 'presence' }
+ 	 	 ,{ field: 'passwordSalt', type: 'presence' }
+ 	 	 ,{ field: 'passwordHash', type: 'presence' }
+ 	 	 ,{ field: 'email', type: 'presence' }
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
