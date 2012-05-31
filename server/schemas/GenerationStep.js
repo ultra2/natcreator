2b8681aa42fdd06006e42d7c8c24fd700ac0bc4b
ultra2 <user10000@freemail.hu>
2012-05-31 13:45:36 -0700

    commit

---
+++ server/schemas/GenerationStep.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var commonPlugin = require('../schemaPlugins/common.js');
+
+exports.GenerationStep = new mongoose.Schema({ 
+		_id: { type: String }
+		,created: { type: Date }
+		,modified: { type: Date }
+		,version: { type: Number }
+		,deleted: { type: Boolean }
+		,generator_id: { type: String, ref: 'Generator' }
+		,path: { type: String }
+		,projectSetting_id: { type: String, ref: 'ProjectSetting' }
+		,deleteDirectory: { type: Boolean }
+		,active: { type: Boolean }
+		,params: { type: String }
+});
+
+exports.GenerationStep.plugin(commonPlugin);
