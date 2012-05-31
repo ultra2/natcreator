48e4c8df8bc53583e05b070d7da2fe54e971f42b
ultra2 <user10000@freemail.hu>
2012-05-30 21:47:47 -0700

    commit

---
+++ server/schemas/StringProperty.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var commonPlugin = require('../schemaPlugins/common.js');
+
+exports.StringProperty = new mongoose.Schema({ 
+		_id: { type: String }
+		,created: { type: Date, default: 'Fri Apr 27 2012 21:45:43 GMT+0200 (Central Europe Daylight Time)' }
+		,modified: { type: Date, default: 'Fri Apr 27 2012 21:45:43 GMT+0200 (Central Europe Daylight Time)' }
+		,version: { type: Number }
+		,deleted: { type: Boolean }
+		,index: { type: Number }
+		,isKey: { type: Boolean }
+		,isDebug: { type: Boolean }
+		,isNullable: { type: Boolean }
+		,isArray: { type: Boolean }
+		,name: { type: String }
+		,isPersistent: { type: Boolean, default: true }
+		,defaultValue: { type: String }
+		,minLength: { type: Number }
+		,maxLength: { type: String }
+		,vFormat: { type: String }
+});
+
+exports.StringProperty.plugin(commonPlugin);
