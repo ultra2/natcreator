5891373baef7d8e05652c27f17452cebdf562871
ultra2 <user10000@freemail.hu>
2012-05-31 13:45:18 -0700

    commit

---
+++ web/models/CRBaseObject.js
@@ -0,0 +1,15 @@
+Ext.define('web.model.CRBaseObject', { 
+ 	 extend: 'natjs.data.NATModel',
+ 	 alias: 'CRBaseObject'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: '_id', type: 'string', debug: true }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
---
+++ web/models/ProjectSetting.js
@@ -0,0 +1,22 @@
+Ext.define('web.model.ProjectSetting', { 
+ 	 extend: 'web.model.PersistentC',
+ 	 alias: 'ProjectSetting'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'repositoryName', type: 'string' }
+ 	 	 ,{ name: 'repositoryHost', type: 'string' }
+ 	 	 ,{ name: 'fontSize', type: 'int' }
+ 	 ]
+ 	 ,associations: [
+ 	 	 { type: 'hasMany', name: 'generationSteps', associationKey: 'generationSteps', model: 'web.model.GenerationStep' , primaryKey: '_id' }
+ 	 ]
+ 	 ,validations: [
+ 	 	 { field: 'repositoryName', type: 'presence' }
+ 	 	 ,{ field: 'repositoryHost', type: 'presence' }
+ 	 	 ,{ field: 'fontSize', type: 'presence' }
+ 	 	 ,{ field: 'generationSteps', type: 'presence' }
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
