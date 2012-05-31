f190f03b40ac01f047543c3f4d59b6babae220b1
ultra2 <user10000@freemail.hu>
2012-05-31 14:39:49 -0700

    commit

---
+++ server/schemas/ProjectSetting.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var commonPlugin = require('../schemaPlugins/common.js');
+var GenerationStep = require('../schemas/GenerationStep.js').GenerationStep;
+
+exports.ProjectSetting = new mongoose.Schema({ 
+		_id: { type: String }
+		,created: { type: Date }
+		,modified: { type: Date }
+		,version: { type: Number }
+		,deleted: { type: Boolean }
+		,repositoryName: { type: String }
+		,repositoryHost: { type: String }
+		,fontSize: { type: Number }
+		,generationSteps: [GenerationStep]
+});
+
+exports.ProjectSetting.plugin(commonPlugin);
