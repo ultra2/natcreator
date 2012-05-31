379e0c38b4d4212ffc7a82a8fd578483694852c0
ultra2 <user10000@freemail.hu>
2012-05-31 14:54:36 -0700

    commit

---
+++ web/models/Communication.js
@@ -0,0 +1,14 @@
+Ext.define('web.model.Communication', { 
+ 	 extend: 'web.model.CRBaseObject',
+ 	 alias: 'Communication'
+ 	 
+ 	 ,fields: [
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
