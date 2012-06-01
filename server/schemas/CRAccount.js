0b4b2c5b126fb51b225138f84db04e39b5925941
ultra2 <user10000@freemail.hu>
2012-06-01 07:57:05 -0700

    commit

---
+++ server/schemas/CRAccount.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var commonPlugin = require('../schemaPlugins/common.js');
+
+exports.CRAccount = new mongoose.Schema({ 
+		_id: { type: String }
+		,created: { type: Date }
+		,modified: { type: Date }
+		,version: { type: Number }
+		,deleted: { type: Boolean }
+		,username: { type: String }
+		,passwordSalt: { type: String }
+		,passwordHash: { type: String }
+		,email: { type: String }
+		,githubToken: { type: String }
+		,resetPasswordKey: { type: String }
+		,isLocal: { type: Boolean }
+		,googleId: { type: String }
+		,lastLoginDate: { type: Date, default: new Date(0) }
+});
+
+exports.CRAccount.plugin(commonPlugin);
