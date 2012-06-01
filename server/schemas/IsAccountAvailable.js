c9513f907adbe9e15183bd8e644414e3b318a867
ultra2 <user10000@freemail.hu>
2012-05-31 17:15:21 -0700

    commit

--- server/schemas/IsAccountAvailable.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var commonPlugin = require('../schemaPlugins/common.js');
-
-exports.IsAccountAvailable = new mongoose.Schema({ 
-		_id: { type: String }
-		,username: { type: String }
-});
-
-exports.IsAccountAvailable.plugin(commonPlugin);
