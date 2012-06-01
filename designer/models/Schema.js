c4ac005bcfc9730158d13c46f196529968a2c249
ultra2 <user10000@freemail.hu>
2012-06-01 12:32:57 -0700

    commit

---
+++ designer/models/Schema.js
@@ -0,0 +1,23 @@
+Ext.define('designer.model.Schema', { 
+ 	 extend: 'designer.model.PersistentProject',
+ 	 alias: 'Schema'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'isHierarchy', type: 'boolean' }
+ 	 	 ,{ name: 'parentId', type: 'string', debug: true }
+ 	 	 ,{ name: 'leaf', type: 'boolean', debug: true }
+ 	 	 ,{ name: 'depth', type: 'int', debug: true }
+ 	 	 ,{ name: 'index', type: 'int', debug: true }
+ 	 	 ,{ name: 'name', type: 'string' }
+ 	 	 ,{ name: 'strParam1', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 	 { type: 'hasMany', name: 'properties', associationKey: 'properties', model: 'designer.model.DataProperty' , primaryKey: '_id' }
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ Ext.data.NodeInterface.decorate(Ext.create('designer.model.Schema'));
+ 
