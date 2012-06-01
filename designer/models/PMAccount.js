4ddd544602c6036a46b0867e7e41b5a969e239b4
ultra2 <user10000@freemail.hu>
2012-06-01 12:35:49 -0700

    commit

---
+++ designer/models/PMAccount.js
@@ -0,0 +1,32 @@
+Ext.define('designer.model.PMAccount', { 
+ 	 extend: 'designer.model.PersistentMaster',
+ 	 alias: 'PMAccount'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'username', type: 'string' }
+ 	 	 ,{ name: 'passwordSalt', type: 'string' }
+ 	 	 ,{ name: 'passwordHash', type: 'string' }
+ 	 	 ,{ name: 'email', type: 'string' }
+ 	 	 ,{ name: 'githubToken', type: 'string' }
+ 	 	 ,{ name: 'resetPasswordKey', type: 'string' }
+ 	 	 ,{ name: 'isLocal', type: 'boolean' }
+ 	 	 ,{ name: 'googleId', type: 'string' }
+ 	 	 ,{ name: 'lastLoginDate', type: 'date', defaultValue: new Date(0) }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 	 { field: 'username', type: 'presence' }
+ 	 	 ,{ field: 'passwordSalt', type: 'presence' }
+ 	 	 ,{ field: 'passwordHash', type: 'presence' }
+ 	 	 ,{ field: 'email', type: 'presence' }
+ 	 	 ,{ field: 'githubToken', type: 'presence' }
+ 	 	 ,{ field: 'resetPasswordKey', type: 'presence' }
+ 	 	 ,{ field: 'isLocal', type: 'presence' }
+ 	 	 ,{ field: 'googleId', type: 'presence' }
+ 	 	 ,{ field: 'lastLoginDate', type: 'presence' }
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
---
+++ designer/models/PMConnection.js
@@ -0,0 +1,24 @@
+Ext.define('designer.model.PMConnection', { 
+ 	 extend: 'designer.model.PersistentMaster',
+ 	 alias: 'PMConnection'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'username', type: 'string' }
+ 	 	 ,{ name: 'password', type: 'string' }
+ 	 	 ,{ name: 'database', type: 'string' }
+ 	 	 ,{ name: 'server', type: 'string' }
+ 	 	 ,{ name: 'port', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 	 { field: 'username', type: 'presence' }
+ 	 	 ,{ field: 'password', type: 'presence' }
+ 	 	 ,{ field: 'database', type: 'presence' }
+ 	 	 ,{ field: 'server', type: 'presence' }
+ 	 	 ,{ field: 'port', type: 'presence' }
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
