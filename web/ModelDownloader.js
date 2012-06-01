cbb9b009f9ee8ac86c8deed9af2e107067536226
ultra2 <user10000@freemail.hu>
2012-06-01 07:43:23 -0700

    commit

--- web/ModelDownloader.js
+++ web/ModelDownloader.js
@@ -1,15 +1,15 @@
 Ext.define('web.ModelDownloader', { 
  	 Download: function() {
  	 	 Ext.syncRequire('web.model.CRBaseObject');
- 	 	 Ext.syncRequire('web.model.Communication');
- 	 	 Ext.syncRequire('web.model.Login');
- 	 	 Ext.syncRequire('web.model.Registration');
- 	 	 Ext.syncRequire('web.model.AccountInfo');
- 	 	 Ext.syncRequire('web.model.IsAccountAvailable');
- 	 	 Ext.syncRequire('web.model.Logout');
- 	 	 Ext.syncRequire('web.model.ResetPassword');
- 	 	 Ext.syncRequire('web.model.ResetPasswordRequest');
- 	 	 Ext.syncRequire('web.model.IsAuthenticated');
+ 	 	 Ext.syncRequire('web.model.Request');
+ 	 	 Ext.syncRequire('web.model.RQLogin');
+ 	 	 Ext.syncRequire('web.model.RQRegistration');
+ 	 	 Ext.syncRequire('web.model.RQAccountInfo');
+ 	 	 Ext.syncRequire('web.model.RQIsAccountAvailable');
+ 	 	 Ext.syncRequire('web.model.RQLogout');
+ 	 	 Ext.syncRequire('web.model.RQResetPassword');
+ 	 	 Ext.syncRequire('web.model.RQResetPasswordRequest');
+ 	 	 Ext.syncRequire('web.model.RQIsAuthenticated');
  	 }
  });
  
