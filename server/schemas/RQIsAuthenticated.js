0c4b172e5766b783d77f8066360ba32134e59679
ultra2 <user10000@freemail.hu>
2012-06-01 13:16:40 -0700

    commit

--- server/schemas/RQIsAuthenticated.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var commonPlugin = require('../schemaPlugins/common.js');
-
-exports.RQIsAuthenticated = new mongoose.Schema({ 
-		_id: { type: String }
-});
-
-exports.RQIsAuthenticated.plugin(commonPlugin);
--- server/schemas/RQLogout.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var commonPlugin = require('../schemaPlugins/common.js');
-
-exports.RQLogout = new mongoose.Schema({ 
-		_id: { type: String }
-});
-
-exports.RQLogout.plugin(commonPlugin);
