ec08d8f94fd94a750e0ae23af99e416f139ad9bf
ultra2 <user10000@freemail.hu>
2012-06-01 12:09:20 -0700

    commit

---
+++ web/models/CRAccountProject.js
@@ -0,0 +1,20 @@
+Ext.define('web.model.CRAccountProject', { 
+ 	 extend: 'web.model.PersistentMaster',
+ 	 alias: 'CRAccountProject'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'account_id', type: 'string' }
+ 	 	 ,{ name: 'project_id', type: 'string' }
+ 	 	 ,{ name: 'role_id', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 	 { field: 'account_id', type: 'presence' }
+ 	 	 ,{ field: 'project_id', type: 'presence' }
+ 	 	 ,{ field: 'role_id', type: 'presence' }
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
