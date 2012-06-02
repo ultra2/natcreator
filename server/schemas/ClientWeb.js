b9c338cb856833c38f6342271fbc50c4b37126a3
ultra2 <user10000@freemail.hu>
2012-06-01 20:15:09 -0700

    commit

--- server/schemas/ClientWeb.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var commonPlugin = require('../schemaPlugins/common.js');
-
-exports.ClientWeb = new mongoose.Schema({ 
-		_id: { type: String }
-});
-
-exports.ClientWeb.plugin(commonPlugin);
