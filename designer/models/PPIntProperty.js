ac81af1f6eda45828b61063b0839a3e86756f533
ultra2 <user10000@freemail.hu>
2012-06-02 06:51:25 -0700

    commit

---
+++ designer/models/PPIntProperty.js
@@ -0,0 +1,15 @@
+Ext.define('designer.model.PPIntProperty', { 
+ 	 extend: 'designer.model.PPDataProperty',
+ 	 alias: 'PPIntProperty'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'defaultValue', type: 'int' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
