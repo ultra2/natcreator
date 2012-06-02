89cd6c16720bdc873d8470900f11e9a312af79cf
ultra2 <user10000@freemail.hu>
2012-06-01 18:27:07 -0700

    commit

--- server/schemas/RQResetPassword.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var commonPlugin = require('../schemaPlugins/common.js');
-
-exports.RQResetPassword = new mongoose.Schema({ 
-		_id: { type: String }
-		,password: { type: String }
-});
-
-exports.RQResetPassword.plugin(commonPlugin);
