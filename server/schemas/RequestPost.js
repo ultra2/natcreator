1dbdfe199fd602ba1aa4eff3fdaf6e08dcb42a5e
ultra2 <user10000@freemail.hu>
2012-06-02 16:31:03 -0700

    commit

--- server/schemas/RequestPost.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var commonPlugin = require('../schemaPlugins/common.js');
-
-exports.RequestPost = new mongoose.Schema({ 
-		_id: { type: String }
-});
-
-exports.RequestPost.plugin(commonPlugin);
