e6058a97ba7e75285b77f6fac5008ab240f2cf82
ultra2 <user10000@freemail.hu>
2012-06-02 10:42:53 -0700

    commit

---
+++ web/models/RQIsAuthenticated.js
@@ -0,0 +1,14 @@
+Ext.define('web.model.RQIsAuthenticated', { 
+ 	 extend: 'web.model.Request',
+ 	 alias: 'RQIsAuthenticated'
+ 	 
+ 	 ,fields: [
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
