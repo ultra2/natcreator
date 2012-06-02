918b7d200cefd6eaf6467929e42afd9fa9236b44
ultra2 <user10000@freemail.hu>
2012-06-01 20:14:57 -0700

    commit

---
+++ web/models/RQLogout.js
@@ -0,0 +1,14 @@
+Ext.define('web.model.RQLogout', { 
+ 	 extend: 'web.model.Request',
+ 	 alias: 'RQLogout'
+ 	 
+ 	 ,fields: [
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
