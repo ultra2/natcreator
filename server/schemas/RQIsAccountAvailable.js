992599f1b97c936b2650c42dc42f2b2ed4de6a5b
ultra2 <user10000@freemail.hu>
2012-06-02 10:50:30 -0700

    commit

--- server/schemas/RQIsAccountAvailable.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var commonPlugin = require('../schemaPlugins/common.js');
-
-exports.RQIsAccountAvailable = new mongoose.Schema({ 
-		_id: { type: String }
-		,username: { type: String }
-});
-
-exports.RQIsAccountAvailable.plugin(commonPlugin);
