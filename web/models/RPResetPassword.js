e3a56ea71b381cf3a6f24a78dce37bd9e57d207e
ultra2 <user10000@freemail.hu>
2012-06-02 17:18:29 -0700

    commit

---
+++ web/models/RPResetPassword.js
@@ -0,0 +1,16 @@
+Ext.define('web.model.RPResetPassword', { 
+ 	 extend: 'web.model.RequestPost',
+ 	 alias: 'RPResetPassword'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'password', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 	 { field: 'password', type: 'presence' }
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
