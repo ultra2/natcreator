b56672608d8e019dee4cf9afcafe7d3c83ede6fe
ultra2 <user10000@freemail.hu>
2012-06-02 17:18:44 -0700

    commit

--- server/schemas/RPIsAuthenticated.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var commonPlugin = require('../schemaPlugins/common.js');
-
-exports.RPIsAuthenticated = new mongoose.Schema({ 
-		_id: { type: String }
-});
-
-exports.RPIsAuthenticated.plugin(commonPlugin);
