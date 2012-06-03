c47fd360f46dfa4283ebb6f7414c957339543ade
ultra2 <user10000@freemail.hu>
2012-06-02 17:06:05 -0700

    commit

--- server/schemas/RQCloseProject.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var commonPlugin = require('../schemaPlugins/common.js');
-
-exports.RQCloseProject = new mongoose.Schema({ 
-		_id: { type: String }
-});
-
-exports.RQCloseProject.plugin(commonPlugin);
--- server/schemas/RQOpenProject.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var commonPlugin = require('../schemaPlugins/common.js');
-
-exports.RQOpenProject = new mongoose.Schema({ 
-		_id: { type: String }
-		,projectId: { type: String }
-});
-
-exports.RQOpenProject.plugin(commonPlugin);
