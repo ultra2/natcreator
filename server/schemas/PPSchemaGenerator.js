df24b21e147f2dbf90dcd9d3fd29e07585da93b7
ultra2 <user10000@freemail.hu>
2012-06-02 17:48:10 -0700

    commit

---
+++ server/schemas/PPSchemaGenerator.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var commonPlugin = require('../schemaPlugins/common.js');
+
+exports.PPSchemaGenerator = new mongoose.Schema({ 
+		_id: { type: String }
+		,created: { type: Date }
+		,modified: { type: Date }
+		,version: { type: Number }
+		,deleted: { type: Boolean }
+		,name: { type: String }
+		,source: { type: String }
+});
+
+exports.PPSchemaGenerator.plugin(commonPlugin);
---
+++ server/schemas/RPTestConnection.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var commonPlugin = require('../schemaPlugins/common.js');
+
+exports.RPTestConnection = new mongoose.Schema({ 
+		_id: { type: String }
+		,username: { type: String }
+		,password: { type: String }
+		,server: { type: String }
+		,database: { type: String }
+		,port: { type: Number }
+});
+
+exports.RPTestConnection.plugin(commonPlugin);
