640347aa8dc291b2a49dc534db490d31b44b377e
ultra2 <user10000@freemail.hu>
2012-06-02 17:45:04 -0700

    commit

---
+++ designer/models/PPBooleanProperty.js
@@ -0,0 +1,15 @@
+Ext.define('designer.model.PPBooleanProperty', { 
+ 	 extend: 'designer.model.PPDataProperty',
+ 	 alias: 'PPBooleanProperty'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'defaultValue', type: 'boolean' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
