c5a79cde6a9bc0d6819917089bc34902f6927cd4
ultra2 <user10000@freemail.hu>
2012-06-06 01:47:57 -0700

    commit

---
+++ server/schemas/RPOpenProject.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var commonPlugin = require('../schemaPlugins/common.js');
+
+exports.RPOpenProject = new mongoose.Schema({ 
+		_id: { type: String }
+		,projectId: { type: String }
+});
+
+exports.RPOpenProject.plugin(commonPlugin);
