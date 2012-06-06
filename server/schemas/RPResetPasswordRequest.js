7b1bc1a0b78b68c6c04a7ef881bc1383a38ab5e5
ultra2 <user10000@freemail.hu>
2012-06-06 09:40:18 -0700

    commit

--- server/schemas/RPResetPasswordRequest.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var commonPlugin = require('../schemaPlugins/common.js');
-
-exports.RPResetPasswordRequest = new mongoose.Schema({ 
-		_id: { type: String }
-		,email: { type: String }
-});
-
-exports.RPResetPasswordRequest.plugin(commonPlugin);
