5bfe588f2d1a5156c681aa2d43ab268e0ea34faa
ultra2 <user10000@freemail.hu>
2012-06-08 23:02:15 -0700

    commit

--- server/schemas/RGActivate.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var commonPlugin = require('../schemaPlugins/common.js');
-
-exports.RGActivate = new mongoose.Schema({ 
-		_id: { type: String }
-		,key: { type: String }
-});
-
-exports.RGActivate.plugin(commonPlugin);
--- server/schemas/RequestObject.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var commonPlugin = require('../schemaPlugins/common.js');
-
-exports.RequestObject = new mongoose.Schema({ 
-		_id: { type: String }
-});
-
-exports.RequestObject.plugin(commonPlugin);
