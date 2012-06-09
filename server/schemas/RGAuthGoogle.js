f9d89ad27fd1bef59c609c70c254e1efccf4c28a
ultra2 <user10000@freemail.hu>
2012-06-09 00:08:45 -0700

    commit

--- server/schemas/RGAuthGoogle.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var commonPlugin = require('../schemaPlugins/common.js');
-
-exports.RGAuthGoogle = new mongoose.Schema({ 
-		_id: { type: String }
-		,_type: { type: String }
-});
-
-exports.RGAuthGoogle.plugin(commonPlugin);
