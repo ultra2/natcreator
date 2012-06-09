4efcbcdb969c767548e2a9152860ebff41c49b93
ultra2 <user10000@freemail.hu>
2012-06-08 22:49:38 -0700

    commit

---
+++ web/models/CDMetaCategoryRow.js
@@ -0,0 +1,15 @@
+Ext.define('web.model.CDMetaCategoryRow', { 
+ 	 extend: 'web.model.CDMetaRow',
+ 	 alias: 'CDMetaCategoryRow'
+ 	 
+ 	 ,fields: [
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ Ext.data.NodeInterface.decorate(Ext.create('web.model.CDMetaCategoryRow'));
+ 
---
+++ web/models/PPProjectSetting.js
@@ -0,0 +1,24 @@
+Ext.define('web.model.PPProjectSetting', { 
+ 	 extend: 'web.model.PersistentProject',
+ 	 alias: 'PPProjectSetting'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'repositoryName', type: 'string' }
+ 	 	 ,{ name: 'repositoryHost', type: 'string' }
+ 	 	 ,{ name: 'fontSize', type: 'int' }
+ 	 	 ,{ name: 'githubToken', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 	 { type: 'hasMany', name: 'generationSteps', associationKey: 'generationSteps', model: 'web.model.PPGenerationStep' , primaryKey: '_id' }
+ 	 ]
+ 	 ,validations: [
+ 	 	 { field: 'repositoryName', type: 'presence' }
+ 	 	 ,{ field: 'repositoryHost', type: 'presence' }
+ 	 	 ,{ field: 'fontSize', type: 'presence' }
+ 	 	 ,{ field: 'generationSteps', type: 'presence' }
+ 	 	 ,{ field: 'githubToken', type: 'presence' }
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
