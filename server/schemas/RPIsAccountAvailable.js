a39b6aab0057016de4557e12101652023ecb59b7
ultra2 <user10000@freemail.hu>
2012-06-08 22:43:42 -0700

    commit

--- server/schemas/RPIsAccountAvailable.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var commonPlugin = require('../schemaPlugins/common.js');
-
-exports.RPIsAccountAvailable = new mongoose.Schema({ 
-		_id: { type: String }
-		,username: { type: String }
-});
-
-exports.RPIsAccountAvailable.plugin(commonPlugin);
