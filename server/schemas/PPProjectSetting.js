5f4814233d922485de8390ecc52eb7b1df678c3f
ultra2 <user10000@freemail.hu>
2012-06-09 21:41:54 -0700

    commit

---
+++ server/schemas/PPProjectSetting.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var commonPlugin = require('../schemaPlugins/common.js');
+var PPGenerationStep = require('../schemas/PPGenerationStep.js').PPGenerationStep;
+
+exports.PPProjectSetting = new mongoose.Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,created: { type: Date }
+		,modified: { type: Date }
+		,version: { type: Number }
+		,deleted: { type: Boolean }
+		,repositoryName: { type: String }
+		,repositoryHost: { type: String }
+		,fontSize: { type: Number }
+		,generationSteps: [PPGenerationStep]
+		,githubToken: { type: String }
+});
+
+exports.PPProjectSetting.plugin(commonPlugin);
