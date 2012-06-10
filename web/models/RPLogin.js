32a50fc0fdada4ca9cbddcc2b7f188aa60e605f9
ultra2 <user10000@freemail.hu>
2012-06-09 21:41:36 -0700

    commit

---
+++ web/models/RPLogin.js
@@ -0,0 +1,19 @@
+Ext.define('web.model.RPLogin', { 
+ 	 extend: 'web.model.RequestPost',
+ 	 alias: 'RPLogin'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'username', type: 'string' }
+ 	 	 ,{ name: 'password', type: 'string' }
+ 	 	 ,{ name: 'rememberMe', type: 'boolean' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 	 { field: 'username', type: 'presence' }
+ 	 	 ,{ field: 'password', type: 'presence' }
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
