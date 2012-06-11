ed6b99f6c6ed0010e628ef22eddc6c371bc1ecd5
ultra2 <user10000@freemail.hu>
2012-06-11 07:41:42 -0700

    commit

---
+++ web/models/RGSetupGithub.js
@@ -0,0 +1,14 @@
+Ext.define('web.model.RGSetupGithub', { 
+ 	 extend: 'web.model.RequestGet',
+ 	 alias: 'RGSetupGithub'
+ 	 
+ 	 ,fields: [
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
