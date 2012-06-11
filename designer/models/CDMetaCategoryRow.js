aea8ac7de1506028757c70b796db35c19ee69077
ultra2 <user10000@freemail.hu>
2012-06-11 07:41:11 -0700

    commit

---
+++ designer/models/CDMetaCategoryRow.js
@@ -0,0 +1,15 @@
+Ext.define('designer.model.CDMetaCategoryRow', { 
+ 	 extend: 'designer.model.CDMetaRow',
+ 	 alias: 'CDMetaCategoryRow'
+ 	 
+ 	 ,fields: [
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ Ext.data.NodeInterface.decorate(Ext.create('designer.model.CDMetaCategoryRow'));
+ 
---
+++ designer/models/RPIsAccountAvailable.js
@@ -0,0 +1,16 @@
+Ext.define('designer.model.RPIsAccountAvailable', { 
+ 	 extend: 'designer.model.RequestPost',
+ 	 alias: 'RPIsAccountAvailable'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'username', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 	 { field: 'username', type: 'presence' }
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
