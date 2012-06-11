7119ca7085c7f18a7315759adab8bba150f3a934
ultra2 <user10000@freemail.hu>
2012-06-11 08:41:21 -0700

    commit

---
+++ server/schemas/PPIntProperty.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var commonPlugin = require('../schemaPlugins/common.js');
+
+exports.PPIntProperty = new mongoose.Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,created: { type: Date }
+		,modified: { type: Date }
+		,version: { type: Number }
+		,deleted: { type: Boolean }
+		,index: { type: Number }
+		,isKey: { type: Boolean }
+		,isDebug: { type: Boolean }
+		,isNullable: { type: Boolean }
+		,isArray: { type: Boolean }
+		,name: { type: String }
+		,isPersistent: { type: Boolean, default: true }
+		,defaultValue: { type: Number }
+		,minValue: { type: Number }
+		,maxValue: { type: Number }
+});
+
+exports.PPIntProperty.plugin(commonPlugin);
