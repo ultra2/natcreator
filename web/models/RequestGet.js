7ebb130fc38f90aa643bece8e76d59aa6e3c590a
ultra2 <user10000@freemail.hu>
2012-06-11 12:03:08 -0700

    commit

---
+++ web/models/RequestGet.js
@@ -0,0 +1,14 @@
+Ext.define('web.model.RequestGet', { 
+ 	 extend: 'web.model.CRBaseObject',
+ 	 alias: 'RequestGet'
+ 	 
+ 	 ,fields: [
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
