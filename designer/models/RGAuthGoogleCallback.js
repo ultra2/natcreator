3e0dc54046d1fdf2b018ae2a00e9853db34d2b39
ultra2 <user10000@freemail.hu>
2012-06-12 10:05:43 -0700

    commit

---
+++ designer/models/RGAuthGoogleCallback.js
@@ -0,0 +1,14 @@
+Ext.define('designer.model.RGAuthGoogleCallback', { 
+ 	 extend: 'designer.model.RequestGet',
+ 	 alias: 'RGAuthGoogleCallback'
+ 	 
+ 	 ,fields: [
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
