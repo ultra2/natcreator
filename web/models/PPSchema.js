4f6e8e353fe7e16c3cf59d0382aba2509207b1ae
ultra2 <user10000@freemail.hu>
2012-06-12 10:06:10 -0700

    commit

---
+++ web/models/PPSchema.js
@@ -0,0 +1,25 @@
+Ext.define('web.model.PPSchema', { 
+ 	 extend: 'web.model.PersistentProject',
+ 	 alias: 'PPSchema'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'isHierarchy', type: 'boolean' }
+ 	 	 ,{ name: 'parentId', type: 'string', debug: true }
+ 	 	 ,{ name: 'leaf', type: 'boolean', debug: true }
+ 	 	 ,{ name: 'depth', type: 'int', debug: true }
+ 	 	 ,{ name: 'index', type: 'int', debug: true }
+ 	 	 ,{ name: 'name', type: 'string' }
+ 	 	 ,{ name: 'strParam1', type: 'string' }
+ 	 	 ,{ name: 'collection', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 	 { type: 'hasMany', name: 'properties', associationKey: 'properties', model: 'web.model.PPDataProperty' , primaryKey: '_id' }
+ 	 ]
+ 	 ,validations: [
+ 	 	 { field: 'collection', type: 'presence' }
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ Ext.data.NodeInterface.decorate(Ext.create('web.model.PPSchema'));
+ 
