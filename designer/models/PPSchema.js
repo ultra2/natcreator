1677a98c2e0b69033aefef5aefe87ba2cee83d3e
ultra2 <user10000@freemail.hu>
2012-06-12 10:18:35 -0700

    commit

---
+++ designer/models/PPSchema.js
@@ -0,0 +1,25 @@
+Ext.define('designer.model.PPSchema', { 
+ 	 extend: 'designer.model.PersistentProject',
+ 	 alias: 'PPSchema'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'isHierarchy', type: 'boolean' }
+ 	 	 ,{ name: 'parentId', type: 'string', debug: true }
+ 	 	 ,{ name: 'leaf', type: 'boolean', debug: true }
+ 	 	 ,{ name: 'depth', type: 'int', debug: true }
+ 	 	 ,{ name: 'index', type: 'int', debug: true }
+ 	 	 ,{ name: 'name', type: 'string' }
+ 	 	 ,{ name: 'strParam1', type: 'string' }
+ 	 	 ,{ name: 'isCollection', type: 'boolean' }
+ 	 ]
+ 	 ,associations: [
+ 	 	 { type: 'hasMany', name: 'properties', associationKey: 'properties', model: 'designer.model.PPDataProperty' , primaryKey: '_id' }
+ 	 ]
+ 	 ,validations: [
+ 	 	 { field: 'isCollection', type: 'presence' }
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ Ext.data.NodeInterface.decorate(Ext.create('designer.model.PPSchema'));
+ 
