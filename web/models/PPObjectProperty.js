123ebf736fbf1925c5d82b11bf47c9a61c687bdd
ultra2 <user10000@freemail.hu>
2012-06-12 16:49:13 -0700

    commit

---
+++ web/models/PPObjectProperty.js
@@ -0,0 +1,15 @@
+Ext.define('web.model.PPObjectProperty', { 
+ 	 extend: 'web.model.PPDataProperty',
+ 	 alias: 'PPObjectProperty'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'type_id', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
