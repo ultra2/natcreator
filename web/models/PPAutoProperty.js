acfbf6db71803b6c97c3f4a1f132bf513be1a3a8
ultra2 <user10000@freemail.hu>
2012-06-13 10:13:21 -0700

    commit

---
+++ web/models/PPAutoProperty.js
@@ -0,0 +1,14 @@
+Ext.define('web.model.PPAutoProperty', { 
+ 	 extend: 'web.model.PPDataProperty',
+ 	 alias: 'PPAutoProperty'
+ 	 
+ 	 ,fields: [
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
