be8d5e203cfe08232d1b0e7e4d23c7cac4a07f45
ultra2 <user10000@freemail.hu>
2012-06-13 01:05:55 -0700

    commit

--- server/schemas/RequestGet.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var commonPlugin = require('../schemaPlugins/common.js');
-
-exports.RequestGet = new mongoose.Schema({ 
-		_id: { type: String }
-		,_type: { type: String }
-});
-
-exports.RequestGet.plugin(commonPlugin);
