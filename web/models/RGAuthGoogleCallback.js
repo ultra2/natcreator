86ea0a1b76620595c97a566148902714337bcdab
ultra2 <user10000@freemail.hu>
2012-06-14 19:45:32 -0700

    commit

---
+++ web/models/RGAuthGoogleCallback.js
@@ -0,0 +1,14 @@
+Ext.define('web.model.RGAuthGoogleCallback', { 
+ 	 extend: 'web.model.RequestGet',
+ 	 alias: 'RGAuthGoogleCallback'
+ 	 
+ 	 ,fields: [
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
