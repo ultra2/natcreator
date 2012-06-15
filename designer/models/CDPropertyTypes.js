2109a301bee21c2bf3d9db174f2c13848193010c
ultra2 <user10000@freemail.hu>
2012-06-14 19:45:05 -0700

    commit

---
+++ designer/models/CDPropertyTypes.js
@@ -0,0 +1,18 @@
+Ext.define('designer.model.CDPropertyTypes', { 
+ 	 extend: 'designer.model.ClientDesigner',
+ 	 alias: 'CDPropertyTypes'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'name', type: 'string' }
+ 	 	 ,{ name: 'displayName', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 	 { field: 'name', type: 'presence' }
+ 	 	 ,{ field: 'displayName', type: 'presence' }
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
