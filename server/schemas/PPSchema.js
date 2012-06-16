b12da4a00de3f7893b1b79f8030add6e394bad56
ultra2 <user10000@freemail.hu>
2012-06-16 09:19:00 -0700

    commit

---
+++ server/schemas/PPSchema.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var commonPlugin = require('../schemaPlugins/common.js');
+var PPDataProperty = require('../schemas/PPDataProperty.js').PPDataProperty;
+
+exports.PPSchema = new mongoose.Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,created: { type: Date }
+		,modified: { type: Date }
+		,version: { type: Number }
+		,deleted: { type: Boolean }
+		,isHierarchy: { type: Boolean }
+		,parentId: { type: String }
+		,leaf: { type: Boolean }
+		,depth: { type: Number }
+		,index: { type: Number }
+		,properties: [PPDataProperty]
+		,name: { type: String }
+		,strParam1: { type: String }
+		,isCollection: { type: Boolean }
+		,displayName: { type: String }
+		,frameworkName: { type: String }
+});
+
+exports.PPSchema.plugin(commonPlugin);
