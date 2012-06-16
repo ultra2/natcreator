4f9d842e6f3d764d017060899ff5ab34c455556a
ultra2 <user10000@freemail.hu>
2012-06-16 09:18:43 -0700

    commit

---
+++ web/models/RPIsAuthenticated.js
@@ -0,0 +1,14 @@
+Ext.define('web.model.RPIsAuthenticated', { 
+ 	 extend: 'web.model.RequestPost',
+ 	 alias: 'RPIsAuthenticated'
+ 	 
+ 	 ,fields: [
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
