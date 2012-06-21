66810cfcd6ecf1d9f5901093af278a4e8fca9aa5
ultra2 <user10000@freemail.hu>
2012-06-21 01:37:36 -0700

    commit

---
+++ web/models/RPLogout.js
@@ -0,0 +1,14 @@
+Ext.define('web.model.RPLogout', { 
+ 	 extend: 'web.model.RequestPost',
+ 	 alias: 'RPLogout'
+ 	 
+ 	 ,fields: [
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
