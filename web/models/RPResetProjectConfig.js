ba5baa81f049c6876b705c35c51799fab3587a9c
ultra2 <user10000@freemail.hu>
2012-06-21 01:31:32 -0700

    commit

---
+++ web/models/RPResetProjectConfig.js
@@ -0,0 +1,14 @@
+Ext.define('web.model.RPResetProjectConfig', { 
+ 	 extend: 'web.model.RequestPost',
+ 	 alias: 'RPResetProjectConfig'
+ 	 
+ 	 ,fields: [
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
