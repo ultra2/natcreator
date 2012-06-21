50d6a3ae64fb464e593a8ae30bc2fe670da20e75
ultra2 <user10000@freemail.hu>
2012-06-21 00:15:34 -0700

    commit

---
+++ web/models/PPReferenceProperty.js
@@ -0,0 +1,15 @@
+Ext.define('web.model.PPReferenceProperty', { 
+ 	 extend: 'web.model.PPDataProperty',
+ 	 alias: 'PPReferenceProperty'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'type_id', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
