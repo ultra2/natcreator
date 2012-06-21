a2ae68655f8ac4b38e76b5fb37a93ced111e3bcc
ultra2 <user10000@freemail.hu>
2012-06-21 01:32:11 -0700

    commit

---
+++ designer/models/RGSetupGithubCallback.js
@@ -0,0 +1,16 @@
+Ext.define('designer.model.RGSetupGithubCallback', { 
+ 	 extend: 'designer.model.RequestGet',
+ 	 alias: 'RGSetupGithubCallback'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'code', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 	 { field: 'code', type: 'presence' }
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
