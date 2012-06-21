8fc9aa7b01cc4401ea7676c4e447ec245e3983b8
ultra2 <user10000@freemail.hu>
2012-06-21 01:29:59 -0700

    commit

---
+++ designer/models/PPObjectProperty.js
@@ -0,0 +1,15 @@
+Ext.define('designer.model.PPObjectProperty', { 
+ 	 extend: 'designer.model.PPDataProperty',
+ 	 alias: 'PPObjectProperty'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'type_id', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
