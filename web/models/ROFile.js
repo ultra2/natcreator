3993aabf3a92bcfffb194ca4e4d34496ecc0ceca
ultra2 <user10000@freemail.hu>
2012-06-21 01:32:49 -0700

    commit

---
+++ web/models/ROFile.js
@@ -0,0 +1,18 @@
+Ext.define('web.model.ROFile', { 
+ 	 extend: 'web.model.RequestObject',
+ 	 alias: 'ROFile'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'content', type: 'string' }
+ 	 	 ,{ name: 'name', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 	 { field: 'content', type: 'presence' }
+ 	 	 ,{ field: 'name', type: 'presence' }
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
