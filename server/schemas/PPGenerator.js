157aa51901ab164512207617eb863260c5b04170
ultra2 <user10000@freemail.hu>
2012-06-23 17:07:11 -0700

    commit

---
+++ server/schemas/PPGenerator.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var commonPlugin = require('../schemaPlugins/common.js');
+
+exports.PPGenerator = new mongoose.Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,created: { type: Date }
+		,modified: { type: Date }
+		,version: { type: Number }
+		,deleted: { type: Boolean }
+		,name: { type: String }
+		,source: { type: String }
+});
+
+exports.PPGenerator.plugin(commonPlugin);
