25bd0807055bb0f9b41c93b99e0ab2ff786e7306
ultra2 <user10000@freemail.hu>
2012-06-23 17:30:10 -0700

    commit

---
+++ web/models/RPOpenProject.js
@@ -0,0 +1,18 @@
+Ext.define('web.model.RPOpenProject', { 
+ 	 extend: 'web.model.RequestPost',
+ 	 alias: 'RPOpenProject'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'projectId', type: 'string' }
+ 	 	 ,{ name: 'mode', type: 'string' }
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 	 { field: 'projectId', type: 'presence' }
+ 	 	 ,{ field: 'mode', type: 'presence' }
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
