3c85cc3caf34d993f9083bf754b2fdcab97cd916
ultra2 <user10000@freemail.hu>
2012-07-12 14:22:09 -0700

    commit

---
+++ server/schemas/PPObjectIdProperty.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var commonPlugin = require('../schemaPlugins/common.js');
+
+exports.PPObjectIdProperty = new mongoose.Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,created: { type: Date }
+		,modified: { type: Date }
+		,version: { type: Number }
+		,deleted: { type: Boolean }
+		,index: { type: Number }
+		,isKey: { type: Boolean }
+		,isDebug: { type: Boolean }
+		,isNullable: { type: Boolean }
+		,isArray: { type: Boolean }
+		,name: { type: String }
+		,isPersistent: { type: Boolean, default: true }
+});
+
+exports.PPObjectIdProperty.plugin(commonPlugin);
