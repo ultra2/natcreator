a9673737c06b1c50514bb7f663d1b8fa29f0e8da
ultra2 <user10000@freemail.hu>
2012-07-12 17:03:10 -0700

    commit

---
+++ server/schemas/CDManagerMenu.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var commonPlugin = require('../schemaPlugins/common.js');
+
+exports.CDManagerMenu = new mongoose.Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,parentId: { type: String }
+		,depth: { type: Number }
+		,leaf: { type: Boolean }
+		,index: { type: Number }
+		,name: { type: String }
+});
+
+exports.CDManagerMenu.plugin(commonPlugin);
---
+++ server/schemas/RGSetupGithub.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var commonPlugin = require('../schemaPlugins/common.js');
+
+exports.RGSetupGithub = new mongoose.Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+});
+
+exports.RGSetupGithub.plugin(commonPlugin);
