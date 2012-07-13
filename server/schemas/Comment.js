fc6d245ee574358796d8f6570abbdba86b1896bd
ultra2 <user10000@freemail.hu>
2012-07-12 18:08:28 -0700

    commit

--- server/schemas/Comment.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose')
-  , Schema = mongoose.Schema;
-
-exports.Comment = new Schema({ 
-		_id: Schema.ObjectId
-		,title: { type: String }
-		,body: { type: String }
-		,date: { type: Date, default: 'Fri Apr 27 2012 00:00:00 GMT+0200 (Central Europe Daylight Time)' }
-});
