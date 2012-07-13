a7d233e904720175f55a0c7832dfc2941d77b142
ultra2 <user10000@freemail.hu>
2012-07-12 17:02:36 -0700

    commit

---
+++ designer/models/ClientDesigner.js
@@ -0,0 +1,14 @@
+Ext.define('designer.model.ClientDesigner', { 
+ 	 extend: 'designer.model.CRBaseObject',
+ 	 alias: 'ClientDesigner'
+ 	 
+ 	 ,fields: [
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
