91f3b73ee45d88f5112be3eabb34161d827fcd16
ultra2 <user10000@freemail.hu>
2012-07-12 17:02:54 -0700

    commit

---
+++ web/models/PPProjectSetting.js
@@ -0,0 +1,26 @@
+Ext.define('web.model.PPProjectSetting', { 
+ 	 extend: 'web.model.PersistentProject',
+ 	 alias: 'PPProjectSetting'
+ 	 
+ 	 ,fields: [
+ 	 	 { name: 'repositoryName', type: 'string' }
+ 	 	 ,{ name: 'repositoryHost', type: 'string' }
+ 	 	 ,{ name: 'fontSize', type: 'int' }
+ 	 	 ,{ name: 'githubUserName', type: 'string' }
+ 	 	 ,{ name: 'codeGenerationWindowCloseOnSuccessful', type: 'boolean' }
+ 	 ]
+ 	 ,associations: [
+ 	 	 { type: 'hasMany', name: 'generationSteps', associationKey: 'generationSteps', model: 'web.model.PPGenerationStep' , primaryKey: '_id' }
+ 	 ]
+ 	 ,validations: [
+ 	 	 { field: 'repositoryName', type: 'presence' }
+ 	 	 ,{ field: 'repositoryHost', type: 'presence' }
+ 	 	 ,{ field: 'fontSize', type: 'presence' }
+ 	 	 ,{ field: 'generationSteps', type: 'presence' }
+ 	 	 ,{ field: 'githubUserName', type: 'presence' }
+ 	 	 ,{ field: 'codeGenerationWindowCloseOnSuccessful', type: 'presence' }
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
---
+++ web/models/RequestObject.js
@@ -0,0 +1,14 @@
+Ext.define('web.model.RequestObject', { 
+ 	 extend: 'web.model.CRBaseObject',
+ 	 alias: 'RequestObject'
+ 	 
+ 	 ,fields: [
+ 	 ]
+ 	 ,associations: [
+ 	 ]
+ 	 ,validations: [
+ 	 ]
+ 	 ,fieldInfos: [
+ 	 ]
+ });
+ 
