87a815a10d1aad1f35c22920b1f98385c55bcd14
ultra2 <user10000@freemail.hu>
2012-07-14 10:13:17 -0700

    commit

--- server/schemas/ROAccountInfo.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose')
-  , Schema = mongoose.Schema;
-
-exports.ROAccountInfo = new Schema({ 
-		_id: { type: String }
-		,_type: { type: String }
-		,username: { type: String }
-		,hasGithubToken: { type: Boolean }
-});
