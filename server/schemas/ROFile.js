430836a6ba6e52fd7f2ed65514bae1cd1ac60cb7
ultra2 <user10000@freemail.hu>
2012-07-14 12:23:35 -0700

    commit

--- server/schemas/ROFile.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose')
-  , Schema = mongoose.Schema;
-
-exports.ROFile = new Schema({ 
-		_id: { type: String }
-		,_type: { type: String }
-		,content: { type: String }
-		,name: { type: String }
-});
--- server/schemas/RequestObject.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose')
-  , Schema = mongoose.Schema;
-
-exports.RequestObject = new Schema({ 
-		_id: { type: String }
-		,_type: { type: String }
-});
