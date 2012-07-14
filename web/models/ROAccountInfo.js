580d775fbaebeabbff4d6c34fbdfb50332456f53
ultra2 <user10000@freemail.hu>
2012-07-14 12:23:23 -0700

    commit

---
+++ web/models/ROAccountInfo.js
@@ -0,0 +1,17 @@
+Ext.define('web.model.ROAccountInfo', { 
+	extend: 'web.model.RequestObject',
+	alias: 'ROAccountInfo'
+	
+	,fields: [
+		{ name: 'username', type: 'string' }
+		,{ name: 'hasGithubToken', type: 'boolean' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'username', type: 'presence' }
+		,{ field: 'hasGithubToken', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
