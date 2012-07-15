98d3149e027d5d769dbeaa4fc506c0a1ef1ea112
ultra2 <user10000@freemail.hu>
2012-07-15 07:38:23 -0700

    commit

---
+++ web/models/PPGenerator.js
@@ -0,0 +1,17 @@
+Ext.define('web.model.PPGenerator', { 
+	extend: 'web.model.PersistentProject',
+	alias: 'PPGenerator'
+	
+	,fields: [
+		{ name: 'name', type: 'string' }
+		,{ name: 'source', type: 'string', debug: true }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'name', type: 'presence' }
+		,{ field: 'source', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
