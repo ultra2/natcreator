88dde42373aa64ad0ea05fa8fd5ab63afef09040
ultra2 <user10000@freemail.hu>
2012-07-15 12:10:40 -0700

    commit

---
+++ designer/models/CDManagerMenu.js
@@ -0,0 +1,24 @@
+Ext.define('designer.model.CDManagerMenu', { 
+	extend: 'designer.model.ClientDesigner',
+	alias: 'CDManagerMenu'
+	
+	,fields: [
+		{ name: 'parentId', type: 'string' }
+		,{ name: 'depth', type: 'int' }
+		,{ name: 'leaf', type: 'boolean' }
+		,{ name: 'index', type: 'int' }
+		,{ name: 'name', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'parentId', type: 'presence' }
+		,{ field: 'depth', type: 'presence' }
+		,{ field: 'leaf', type: 'presence' }
+		,{ field: 'index', type: 'presence' }
+		,{ field: 'name', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
+Ext.data.NodeInterface.decorate(Ext.create('designer.model.CDManagerMenu'));
---
+++ designer/models/PPBooleanProperty.js
@@ -0,0 +1,14 @@
+Ext.define('designer.model.PPBooleanProperty', { 
+	extend: 'designer.model.PPDataProperty',
+	alias: 'PPBooleanProperty'
+	
+	,fields: [
+		{ name: 'defaultValue', type: 'boolean' }
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
