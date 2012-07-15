69a0c06019818c38f8964e11d005312071366ae6
ultra2 <user10000@freemail.hu>
2012-07-15 11:51:50 -0700

    commit

---
+++ server/schemas/PMSharedGenerator.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose')
+   ,Schema = mongoose.Schema
+   ,commonPlugin = require('../schemaPlugins/common.js');
+
+exports.PMSharedGenerator = new Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,created: { type: Date }
+		,modified: { type: Date }
+		,version: { type: Number }
+		,deleted: { type: Boolean }
+		,name: { type: String }
+});
+exports.PMSharedGenerator.plugin(commonPlugin);
