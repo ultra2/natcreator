9be341d50c0b3a81b68db86d8545030155fe2457
ultra2 <user10000@freemail.hu>
2012-08-10 00:04:56 -0700

    commit

---
+++ web/models/PPMetaProperty.js
@@ -0,0 +1,21 @@
+Ext.define('web.model.PPMetaProperty', { 
+	extend: 'web.model.PersistentProject',
+	alias: 'PPMetaProperty'
+	
+	,fields: [
+		{ name: 'frameworkItem', type: 'string' }
+		,{ name: 'type', type: 'string' }
+		,{ name: 'name', type: 'string' }
+		,{ name: 'defaultValue', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'frameworkItem', type: 'presence' }
+		,{ field: 'type', type: 'presence' }
+		,{ field: 'name', type: 'presence' }
+		,{ field: 'defaultValue', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
---
+++ web/models/PPSchema.js
@@ -0,0 +1,28 @@
+Ext.define('web.model.PPSchema', { 
+	extend: 'web.model.PersistentProject',
+	alias: 'PPSchema'
+	
+	,fields: [
+		{ name: 'isHierarchy', type: 'boolean' }
+		,{ name: 'parentId', type: 'string', debug: true }
+		,{ name: 'leaf', type: 'boolean', debug: true }
+		,{ name: 'depth', type: 'int', debug: true }
+		,{ name: 'index', type: 'int', debug: true }
+		,{ name: 'name', type: 'string' }
+		,{ name: 'strParam1', type: 'string' }
+		,{ name: 'isCollection', type: 'boolean' }
+		,{ name: 'displayName', type: 'string' }
+		,{ name: 'frameworkName', type: 'string' }
+	]
+	,associations: [
+		{ type: 'hasMany', name: 'properties', associationKey: 'properties', model: 'web.model.PPDataProperty' , primaryKey: '_id' }
+	]
+	,validations: [
+		{ field: 'isCollection', type: 'presence' }
+		,{ field: 'displayName', type: 'presence' }
+		,{ field: 'frameworkName', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
+Ext.data.NodeInterface.decorate(Ext.create('web.model.PPSchema'));
