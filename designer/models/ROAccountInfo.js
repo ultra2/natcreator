e0484cc23669965d64b1ffff5ac10de3883aeb80
ultra2 <user10000@freemail.hu>
2012-08-10 03:43:52 -0700

    commit

---
+++ designer/models/ROAccountInfo.js
@@ -0,0 +1,17 @@
+Ext.define('designer.model.ROAccountInfo', { 
+	extend: 'designer.model.RequestObject',
+	alias: 'ROAccountInfo'
+	
+	,fields: [
+		{ name: 'username', type: 'string' }
+		,{ name: 'hasGithubToken', type: 'boolean' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'username', type: 'presence' }
+		,{ field: 'hasGithubToken', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
