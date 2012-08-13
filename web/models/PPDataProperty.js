0652269077591c5c6b979fc09835af1d43641e59
ultra2 <user10000@freemail.hu>
2012-08-13 02:24:25 -0700

    commit

---
+++ web/models/PPDataProperty.js
@@ -0,0 +1,18 @@
+Ext.define('web.model.PPDataProperty', { 
+	extend: 'web.model.PersistentProject',
+	alias: 'PPDataProperty'
+	
+	,fields: [
+		{ name: 'isKey', type: 'boolean' }
+		,{ name: 'isNullable', type: 'boolean' }
+		,{ name: 'isArray', type: 'boolean' }
+		,{ name: 'name', type: 'string' }
+		,{ name: 'isPersistent', type: 'boolean', defaultValue: true }
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
