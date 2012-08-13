a12f1ecee3516d4979850dfec87652402e646fab
ultra2 <user10000@freemail.hu>
2012-08-13 02:24:41 -0700

    commit

---
+++ server/schemas/PPReferenceProperty.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose')
+   ,Schema = mongoose.Schema
+   ,commonPlugin = require('../schemaPlugins/common.js');
+
+exports.PPReferenceProperty = new Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,created: { type: Date }
+		,modified: { type: Date }
+		,version: { type: Number }
+		,deleted: { type: Boolean }
+		,isKey: { type: Boolean }
+		,isNullable: { type: Boolean }
+		,isArray: { type: Boolean }
+		,name: { type: String }
+		,isPersistent: { type: Boolean, default: true }
+		,type_id: { type: String, ref: 'PPSchema' }
+});
+exports.PPReferenceProperty.plugin(commonPlugin);
