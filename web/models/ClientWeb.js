7187ee0512abb82a54b21ce119af5e6b636b3272
ultra2 <user10000@freemail.hu>
2012-08-14 02:03:24 -0700

    commit

---
+++ web/models/ClientWeb.js
@@ -0,0 +1,13 @@
+Ext.define('web.model.ClientWeb', { 
+	extend: 'web.model.CRBaseObject',
+	alias: 'ClientWeb'
+	
+	,fields: [
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
