57ca3d8d87d5dc4461e310501867c9ffcd963da7
ultra2 <user10000@freemail.hu>
2012-08-21 19:59:03 -0700

    commit

---
+++ web/models/RPInstallGenerator.js
@@ -0,0 +1,15 @@
+Ext.define('web.model.RPInstallGenerator', { 
+	extend: 'web.model.RequestPost',
+	alias: 'RPInstallGenerator'
+	
+	,fields: [
+		{ name: 'sharedGeneratorId', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'sharedGeneratorId', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
