07ced4f410d73b78eec1befcf9a6d078b9681cfa
ultra2 <user10000@freemail.hu>
2012-08-21 20:02:45 -0700

    commit

---
+++ server/schemas/PPGenerationStep.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose')
+   ,Schema = mongoose.Schema
+   ,commonPlugin = require('../schemaPlugins/common.js');
+
+exports.PPGenerationStep = new Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,created: { type: Date }
+		,modified: { type: Date }
+		,version: { type: Number }
+		,deleted: { type: Boolean }
+		,generator_id: { type: String, ref: 'PPGenerator' }
+		,path: { type: String, default: '/' }
+		,projectSetting_id: { type: String, ref: 'PPProjectSetting' }
+		,deleteDirectory: { type: Boolean, default: true }
+		,active: { type: Boolean, default: true }
+		,params: { type: String }
+});
+exports.PPGenerationStep.plugin(commonPlugin);
