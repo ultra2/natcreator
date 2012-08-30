268a94f44f09409f7a15b0c5704f632dab5704ad
ultra2 <user10000@freemail.hu>
2012-08-30 16:42:39 -0700

    commit

---
+++ web/models/PMConnection.js
@@ -0,0 +1,25 @@
+Ext.define('web.model.PMConnection', { 
+	extend: 'web.model.PersistentMaster',
+	alias: 'PMConnection'
+	
+	,fields: [
+		{ name: 'username', type: 'string' }
+		,{ name: 'password', type: 'string' }
+		,{ name: 'database', type: 'string' }
+		,{ name: 'server', type: 'string' }
+		,{ name: 'port', type: 'int' }
+		,{ name: 'url', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'username', type: 'presence' }
+		,{ field: 'password', type: 'presence' }
+		,{ field: 'database', type: 'presence' }
+		,{ field: 'server', type: 'presence' }
+		,{ field: 'port', type: 'presence' }
+		,{ field: 'url', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
