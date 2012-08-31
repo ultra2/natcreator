2bbdafeb24b16c664b59c7182e21a20ec639d5d1
ultra2 <user10000@freemail.hu>
2012-08-31 08:02:25 -0700

    commit

---
+++ server/schemas/PMProject.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose')
+   ,Schema = mongoose.Schema
+   ,commonPlugin = require('../schemaPlugins/common.js');
+var PMConnection = require('../schemas/PMConnection.js').PMConnection;
+
+exports.PMProject = new Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,created: { type: Date }
+		,modified: { type: Date }
+		,version: { type: Number }
+		,deleted: { type: Boolean }
+		,connections: [PMConnection]
+		,name: { type: String }
+		,description: { type: String }
+		,isTemplate: { type: Boolean }
+		,template_id: { type: String, ref: 'PMProject' }
+});
+exports.PMProject.plugin(commonPlugin);
