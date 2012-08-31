6255eebd48f019ddac455257f4011abb3ef8ceb0
ultra2 <user10000@freemail.hu>
2012-08-31 15:47:38 -0700

    commit

---
+++ web/models/RGResetPassword.js
@@ -0,0 +1,15 @@
+Ext.define('web.model.RGResetPassword', { 
+	extend: 'web.model.RequestGet',
+	alias: 'RGResetPassword'
+	
+	,fields: [
+		{ name: 'key', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'key', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
