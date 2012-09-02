618049163f9dab3457f128914955b4595e2572c4
ultra2 <user10000@freemail.hu>
2012-09-02 07:37:39 -0700

    commit

---
+++ server/schemas/PPGenerator.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose')
+   ,Schema = mongoose.Schema
+   ,commonPlugin = require('../schemaPlugins/common.js');
+
+exports.PPGenerator = new Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,created: { type: Date }
+		,modified: { type: Date }
+		,version: { type: Number }
+		,deleted: { type: Boolean }
+		,name: { type: String }
+		,source: { type: String, default: 'function Generator(params) \n{ \n\tthis.params = params; \n} \n \nGenerator.prototype.getType = function() \n{ \n\treturn 'schema'; \n}; \n\nGenerator.prototype.getRequiredCustomProperties = function() \n{ \n\treturn []; \n}; \n\nGenerator.prototype.run = function(schema, file, errors) \n{' }
+		,origin_id: { type: String, ref: 'PMSharedGenerator' }
+		,originRepo: { type: String }
+		,originPath: { type: String }
+});
+exports.PPGenerator.plugin(commonPlugin);
