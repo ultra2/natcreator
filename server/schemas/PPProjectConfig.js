0e9a5a255630fdfb0292893a3e5753b7ba3dbd1e
ultra2 <user10000@freemail.hu>
2012-09-02 07:16:12 -0700

    commit

---
+++ server/schemas/PPProjectConfig.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose')
+   ,Schema = mongoose.Schema
+   ,commonPlugin = require('../schemaPlugins/common.js');
+var PPCustomProperty = require('../schemas/PPCustomProperty.js').PPCustomProperty;
+
+exports.PPProjectConfig = new Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,created: { type: Date }
+		,modified: { type: Date }
+		,version: { type: Number }
+		,deleted: { type: Boolean }
+		,customProperties: [PPCustomProperty]
+});
+exports.PPProjectConfig.plugin(commonPlugin);
