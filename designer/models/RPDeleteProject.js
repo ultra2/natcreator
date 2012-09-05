6ca8777c9f071e134a8681adbe90078040b1bc2a
ultra2 <user10000@freemail.hu>
2012-09-05 07:36:12 -0700

    commit

---
+++ designer/models/RPDeleteProject.js
@@ -0,0 +1,15 @@
+Ext.define('designer.model.RPDeleteProject', { 
+	extend: 'designer.model.RequestPost',
+	alias: 'RPDeleteProject'
+	
+	,fields: [
+		{ name: 'projectId', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'projectId', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
