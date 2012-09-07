e7627afde7334809d36ac33c24eec7de941aecbc
ultra2 <user10000@freemail.hu>
2012-09-07 16:55:03 -0700

    commit

---
+++ web/models/PMProject.js
@@ -0,0 +1,24 @@
+Ext.define('web.model.PMProject', { 
+	extend: 'web.model.PersistentMaster',
+	alias: 'PMProject'
+	
+	,fields: [
+		{ name: 'name', type: 'string' }
+		,{ name: 'description', type: 'string' }
+		,{ name: 'isTemplate', type: 'boolean' }
+		,{ name: 'template_id', type: 'string' }
+		,{ name: 'guide', type: 'string' }
+	]
+	,associations: [
+		{ type: 'hasMany', name: 'connections', associationKey: 'connections', model: 'web.model.PMConnection' , primaryKey: '_id' }
+	]
+	,validations: [
+		{ field: 'connections', type: 'presence' }
+		,{ field: 'name', type: 'presence' }
+		,{ field: 'name', type: 'length', min: 6, max: 30 }
+		,{ field: 'description', type: 'presence' }
+		,{ field: 'template_id', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
