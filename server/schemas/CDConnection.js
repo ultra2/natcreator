987093e5dc9aaa66154945dc8800586e94a1c9e4
ultra2 <user10000@freemail.hu>
2012-09-10 03:46:05 -0700

    commit

---
+++ server/schemas/CDConnection.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose')
+   ,Schema = mongoose.Schema
+   ,commonPlugin = require('../schemaPlugins/common.js');
+
+exports.CDConnection = new Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,username: { type: String }
+		,password: { type: String }
+		,database: { type: String }
+		,server: { type: String }
+		,port: { type: Number }
+});
+exports.CDConnection.plugin(commonPlugin);
