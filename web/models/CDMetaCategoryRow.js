bb4c1e05105419e823f555d7d50f78812013b58e
ultra2 <user10000@freemail.hu>
2012-09-10 03:45:59 -0700

    commit

---
+++ web/models/CDMetaCategoryRow.js
@@ -0,0 +1,14 @@
+Ext.define('web.model.CDMetaCategoryRow', { 
+	extend: 'web.model.CDMetaRow',
+	alias: 'CDMetaCategoryRow'
+	
+	,fields: [
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
+Ext.data.NodeInterface.decorate(Ext.create('web.model.CDMetaCategoryRow'));
---
+++ web/models/RPGetTemplateFiles.js
@@ -0,0 +1,13 @@
+Ext.define('web.model.RPGetTemplateFiles', { 
+	extend: 'web.model.RequestPost',
+	alias: 'RPGetTemplateFiles'
+	
+	,fields: [
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
