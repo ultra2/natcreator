d2c82b64f5e58496a2aa95b4e8cfa36847911b08
ultra2 <user10000@freemail.hu>
2012-09-10 17:14:31 -0700

    commit

---
+++ designer/models/ROFile.js
@@ -0,0 +1,17 @@
+Ext.define('designer.model.ROFile', { 
+	extend: 'designer.model.RequestObject',
+	alias: 'ROFile'
+	
+	,fields: [
+		{ name: 'content', type: 'string' }
+		,{ name: 'name', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'content', type: 'presence' }
+		,{ field: 'name', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
