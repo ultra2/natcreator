da5a75e07cf746a52221522883968e1aaedf70f3
ultra2 <user10000@freemail.hu>
2012-09-11 14:03:45 -0700

    commit

---
+++ designer/models/RGActivate.js
@@ -0,0 +1,15 @@
+Ext.define('designer.model.RGActivate', { 
+	extend: 'designer.model.RequestGet',
+	alias: 'RGActivate'
+	
+	,fields: [
+		{ name: 'key', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'key', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
