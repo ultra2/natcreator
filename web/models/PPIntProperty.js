8ee62206249cd60e8eb9efbf3a8a0085b8bcead9
ultra2 <user10000@freemail.hu>
2012-09-11 17:26:25 -0700

    commit

---
+++ web/models/PPIntProperty.js
@@ -0,0 +1,16 @@
+Ext.define('web.model.PPIntProperty', { 
+	extend: 'web.model.PPDataProperty',
+	alias: 'PPIntProperty'
+	
+	,fields: [
+		{ name: 'defaultValue', type: 'int' }
+		,{ name: 'minValue', type: 'int' }
+		,{ name: 'maxValue', type: 'int' }
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
