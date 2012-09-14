fc13927b10219552044181c2467680612b592564
ultra2 <user10000@freemail.hu>
2012-09-14 11:15:56 -0700

    commit

--- server/schemas/RGGetTemplateFiles.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose')
-   ,Schema = mongoose.Schema
-   ,commonPlugin = require('../schemaPlugins/common.js');
-
-exports.RGGetTemplateFiles = new Schema({ 
-		_id: { type: String }
-		,_type: { type: String }
-});
-exports.RGGetTemplateFiles.plugin(commonPlugin);
