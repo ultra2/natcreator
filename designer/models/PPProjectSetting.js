baa6c5857c18a518f28cb47f45aff3bb16e18606
ultra2 <user10000@freemail.hu>
2012-09-14 09:49:03 -0700

    commit

---
+++ designer/models/PPProjectSetting.js
@@ -0,0 +1,24 @@
+Ext.define('designer.model.PPProjectSetting', { 
+	extend: 'designer.model.PersistentProject',
+	alias: 'PPProjectSetting'
+	
+	,fields: [
+		{ name: 'generationTarget', type: 'string', defaultValue: 'zip' }
+		,{ name: 'githubUser', type: 'string' }
+		,{ name: 'githubRepository', type: 'string' }
+		,{ name: 'codeGenerationWindowCloseOnSuccessful', type: 'boolean' }
+		,{ name: 'includeTemplateFilesIntoGeneration', type: 'boolean', defaultValue: true }
+		,{ name: 'fontSize', type: 'int', defaultValue: '18' }
+	]
+	,associations: [
+		{ type: 'hasMany', name: 'generationSteps', associationKey: 'generationSteps', model: 'designer.model.PPGenerationStep' , primaryKey: '_id' }
+	]
+	,validations: [
+		{ field: 'generationTarget', type: 'presence' }
+		,{ field: 'generationSteps', type: 'presence' }
+		,{ field: 'includeTemplateFilesIntoGeneration', type: 'presence' }
+		,{ field: 'fontSize', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
