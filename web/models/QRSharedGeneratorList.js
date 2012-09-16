34c4914c9edeaaea78d7374f75fe0133b692b34b
ultra2 <user10000@freemail.hu>
2012-09-15 19:07:08 -0700

    commit

---
+++ web/models/QRSharedGeneratorList.js
@@ -0,0 +1,17 @@
+Ext.define('web.model.QRSharedGeneratorList', { 
+	extend: 'web.model.QueryResult',
+	alias: 'QRSharedGeneratorList'
+	
+	,fields: [
+		{ name: 'name', type: 'string' }
+		,{ name: 'generatorVersion', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'name', type: 'presence' }
+		,{ field: 'generatorVersion', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
