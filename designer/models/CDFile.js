0cdfc21a2c2b8ec31ad23a31e2aa2418429e3a64
ultra2 <user10000@freemail.hu>
2012-09-15 18:18:53 -0700

    commit

---
+++ designer/models/CDFile.js
@@ -0,0 +1,17 @@
+Ext.define('designer.model.CDFile', { 
+	extend: 'designer.model.ClientDesigner',
+	alias: 'CDFile'
+	
+	,fields: [
+		{ name: 'name', type: 'string' }
+		,{ name: 'content', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'name', type: 'presence' }
+		,{ field: 'content', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
---
+++ designer/models/RequestObject.js
@@ -0,0 +1,13 @@
+Ext.define('designer.model.RequestObject', { 
+	extend: 'designer.model.CRBaseObject',
+	alias: 'RequestObject'
+	
+	,fields: [
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
