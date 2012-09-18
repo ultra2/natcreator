914768a57a0ddbee7b7790b88a844c99a0a54c54
ultra2 <user10000@freemail.hu>
2012-09-18 12:21:16 -0700

    commit

---
+++ web/models/RPResetPasswordRequest.js
@@ -0,0 +1,16 @@
+Ext.define('web.model.RPResetPasswordRequest', { 
+	extend: 'web.model.RequestPost',
+	alias: 'RPResetPasswordRequest'
+	
+	,fields: [
+		{ name: 'email', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'email', type: 'presence' }
+		,{ field: 'email', type: 'email' }
+	]
+	,fieldInfos: [
+	]
+});
