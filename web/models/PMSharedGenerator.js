0ddb1e93a377b8576364bd6f95070d91e594b8df
ultra2 <user10000@freemail.hu>
2012-09-18 11:53:00 -0700

    commit

---
+++ web/models/PMSharedGenerator.js
@@ -0,0 +1,21 @@
+Ext.define('web.model.PMSharedGenerator', { 
+	extend: 'web.model.PersistentMaster',
+	alias: 'PMSharedGenerator'
+	
+	,fields: [
+		{ name: 'name', type: 'string' }
+		,{ name: 'originProject_id', type: 'string' }
+		,{ name: 'originGenerator_id', type: 'string' }
+		,{ name: 'generatorVersion', type: 'string', defaultValue: '1.0' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'name', type: 'presence' }
+		,{ field: 'originProject_id', type: 'presence' }
+		,{ field: 'originGenerator_id', type: 'presence' }
+		,{ field: 'generatorVersion', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
---
+++ web/models/RPCloseProject.js
@@ -0,0 +1,13 @@
+Ext.define('web.model.RPCloseProject', { 
+	extend: 'web.model.RequestPost',
+	alias: 'RPCloseProject'
+	
+	,fields: [
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
