ad4ff5ff777b23d9d0fb5b7cbe68bc61ed6a3c30
ultra2 <user10000@freemail.hu>
2012-09-27 08:21:35 -0700

    commit

---
+++ server/schemas/PPView.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose')
+   ,Schema = mongoose.Schema
+   ,commonPlugin = require('../schemaPlugins/common.js');
+
+exports.PPView = new Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,created: { type: Date }
+		,modified: { type: Date }
+		,version: { type: Number }
+		,deleted: { type: Boolean }
+		,name: { type: String }
+		,json: { type: mongoose.Schema.Types.Mixed }
+});
+exports.PPView.plugin(commonPlugin);
