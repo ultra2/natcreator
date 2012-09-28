6105a5646ad48d75bec7416cf52f2c0c80a339db
ultra2 <user10000@freemail.hu>
2012-09-28 10:49:15 -0700

    commit

---
+++ designer/models/RPExportFiles.js
@@ -0,0 +1,19 @@
+Ext.define('designer.model.RPExportFiles', { 
+	extend: 'designer.model.RequestPost',
+	alias: 'RPExportFiles'
+	
+	,fields: [
+		{ name: 'path', type: 'string' }
+		,{ name: 'deleteDirectory', type: 'boolean' }
+	]
+	,associations: [
+		{ type: 'hasMany', name: 'files', associationKey: 'files', model: 'designer.model.ROFile' , primaryKey: '_id' }
+	]
+	,validations: [
+		{ field: 'path', type: 'presence' }
+		,{ field: 'deleteDirectory', type: 'presence' }
+		,{ field: 'files', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
