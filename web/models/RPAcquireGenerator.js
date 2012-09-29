4df22a01d90c99cee34d2a7ec68b340c916700a9
ultra2 <user10000@freemail.hu>
2012-09-29 14:00:46 -0700

    commit

---
+++ web/models/RPAcquireGenerator.js
@@ -0,0 +1,15 @@
+Ext.define('web.model.RPAcquireGenerator', { 
+	extend: 'web.model.RequestPost',
+	alias: 'RPAcquireGenerator'
+	
+	,fields: [
+		{ name: 'generatorId', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'generatorId', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
