62398af3185e39f3d9ec95cb14f03bb2ba6b021d
ultra2 <user10000@freemail.hu>
2012-10-03 17:54:05 -0700

    commit

---
+++ designer/models/CDViewNode.js
@@ -0,0 +1,18 @@
+Ext.define('designer.model.CDViewNode', { 
+	extend: 'designer.model.ClientDesigner',
+	alias: 'CDViewNode'
+	
+	,fields: [
+		{ name: 'name', type: 'string' }
+		,{ name: 'type', type: 'string' }
+		,{ name: 'userConfig', type: 'auto' }
+		,{ name: 'masterInstanceId_id', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
+Ext.data.NodeInterface.decorate(Ext.create('designer.model.CDViewNode'));
