15fb5939f017cfbac2f31226d93cceb741008589
ultra2 <user10000@freemail.hu>
2012-10-03 17:54:11 -0700

    commit

---
+++ server/schemas/CDViewNode.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose')
+   ,Schema = mongoose.Schema
+   ,commonPlugin = require('../schemaPlugins/common.js');
+
+exports.CDViewNode = new Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,name: { type: String }
+		,type: { type: String }
+		,userConfig: { type: mongoose.Schema.Types.Mixed }
+		,masterInstanceId_id: { type: String, ref: 'CDViewNode' }
+});
+exports.CDViewNode.plugin(commonPlugin);
