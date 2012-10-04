d4aa3ece7e665a783f88a7ce97dee7d219978889
ultra2 <user10000@freemail.hu>
2012-10-03 22:32:18 -0700

    commit

---
+++ designer/models/CDViewNode.js
@@ -0,0 +1,22 @@
+Ext.define('designer.model.CDViewNode', { 
+	extend: 'designer.model.ClientDesigner',
+	alias: 'CDViewNode'
+	
+	,fields: [
+		{ name: 'name', type: 'string' }
+		,{ name: 'type', type: 'string' }
+		,{ name: 'reference', type: 'auto' }
+		,{ name: 'codeClass', type: 'auto' }
+		,{ name: 'userConfig', type: 'auto' }
+		,{ name: 'customConfigs', type: 'auto' }
+		,{ name: 'expanded', type: 'boolean' }
+		,{ name: 'masterInstanceId', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
+Ext.data.NodeInterface.decorate(Ext.create('designer.model.CDViewNode'));
---
+++ designer/models/ClientWeb.js
@@ -0,0 +1,13 @@
+Ext.define('designer.model.ClientWeb', { 
+	extend: 'designer.model.CRBaseObject',
+	alias: 'ClientWeb'
+	
+	,fields: [
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
