9d002d288eb0e0ba009e15583c8769368b6339c4
ultra2 <user10000@freemail.hu>
2012-10-03 23:47:43 -0700

    commit

---
+++ web/models/CDPropertyNode.js
@@ -0,0 +1,18 @@
+Ext.define('web.model.CDPropertyNode', { 
+	extend: 'web.model.ClientDesigner',
+	alias: 'CDPropertyNode'
+	
+	,fields: [
+		{ name: 'name', type: 'string' }
+		,{ name: 'type', type: 'string' }
+		,{ name: 'value', type: 'auto' }
+		,{ name: 'values', type: 'auto' }
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
+Ext.data.NodeInterface.decorate(Ext.create('web.model.CDPropertyNode'));
