455c04bf7c221b36e8a5e56d3090df47fbe8b064
ultra2 <user10000@freemail.hu>
2012-10-11 04:32:52 -0700

    commit

---
+++ server/schemas/CDClass.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose')
+   ,Schema = mongoose.Schema
+   ,commonPlugin = require('../schemaPlugins/common.js');
+var CDClassConfig = require('../schemas/CDClassConfig.js').CDClassConfig;
+
+exports.CDClass = new Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,mixins: { type: mongoose.Schema.Types.Mixed }
+		,displayName: { type: String }
+		,iconIndex: { type: Number }
+		,dropables: { type: mongoose.Schema.Types.Mixed }
+		,configs: [CDClassConfig]
+});
+exports.CDClass.plugin(commonPlugin);
