05c8a0042fac0d7dae26b8ef41e1f175ce982624
ultra2 <user10000@freemail.hu>
2012-10-12 00:38:48 -0700

    commit

---
+++ web/models/PPView.js
@@ -0,0 +1,18 @@
+Ext.define('web.model.PPView', { 
+	extend: 'web.model.PersistentProject',
+	alias: 'PPView'
+	
+	,fields: [
+		{ name: 'name', type: 'string' }
+		,{ name: 'alias', type: 'string' }
+		,{ name: 'meta', type: 'auto' }
+		,{ name: 'config', type: 'auto', persist: false }
+		,{ name: 'source', type: 'string', persist: false }
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
