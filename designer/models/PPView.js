41003c30b86480a36901a946292b268db4e3536a
ultra2 <user10000@freemail.hu>
2012-10-12 00:38:44 -0700

    commit

---
+++ designer/models/PPView.js
@@ -0,0 +1,18 @@
+Ext.define('designer.model.PPView', { 
+	extend: 'designer.model.PersistentProject',
+	alias: 'PPView'
+	
+	,fields: [
+		{ name: 'name', type: 'string' }
+		,{ name: 'alias', type: 'string' }
+		,{ name: 'meta', type: 'auto' }
+		,{ name: 'config', type: 'auto', persist: false }
+		,{ name: 'source', type: 'string', persist: false }
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
