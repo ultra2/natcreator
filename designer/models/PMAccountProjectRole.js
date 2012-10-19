5c380758d292d2f6e88b8ddb6b7c51d0cb3ec4e9
ultra2 <user10000@freemail.hu>
2012-10-19 11:44:27 -0700

    commit

---
+++ designer/models/PMAccountProjectRole.js
@@ -0,0 +1,15 @@
+Ext.define('designer.model.PMAccountProjectRole', { 
+	extend: 'designer.model.PersistentMaster',
+	alias: 'PMAccountProjectRole'
+	
+	,fields: [
+		{ name: 'name', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'name', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
