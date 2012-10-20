9c7706aeb5b245a9138cbef4b2dfecd34e8867d0
ultra2 <user10000@freemail.hu>
2012-10-20 10:08:51 -0700

    commit

---
+++ web/models/RPCreateCustomProperty.js
@@ -0,0 +1,20 @@
+Ext.define('web.model.RPCreateCustomProperty', { 
+	extend: 'web.model.RequestPost',
+	alias: 'RPCreateCustomProperty'
+	
+	,fields: [
+		{ name: 'projectItem', type: 'string' }
+		,{ name: 'type', type: 'string' }
+		,{ name: 'name', type: 'string' }
+		,{ name: 'defaultValue', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'projectItem', type: 'presence' }
+		,{ field: 'type', type: 'presence' }
+		,{ field: 'name', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
