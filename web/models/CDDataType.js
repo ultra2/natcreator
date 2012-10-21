24e11027e5d579c6d4aeddaaa00af8044c15a01b
ultra2 <user10000@freemail.hu>
2012-10-21 15:02:21 -0700

    commit

---
+++ web/models/CDDataType.js
@@ -0,0 +1,15 @@
+Ext.define('web.model.CDDataType', { 
+	extend: 'web.model.ClientDesigner',
+	alias: 'CDDataType'
+	
+	,fields: [
+		{ name: 'name', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'name', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
