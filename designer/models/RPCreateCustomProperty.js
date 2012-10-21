8a0e25c95db785e0b7f4484c0cd206c0f26fc64c
ultra2 <user10000@freemail.hu>
2012-10-21 14:58:42 -0700

    commit

---
+++ designer/models/RPCreateCustomProperty.js
@@ -0,0 +1,21 @@
+Ext.define('designer.model.RPCreateCustomProperty', { 
+	extend: 'designer.model.RequestPost',
+	alias: 'RPCreateCustomProperty'
+	
+	,fields: [
+		{ name: 'projectItem', type: 'string' }
+		,{ name: 'type', type: 'string' }
+		,{ name: 'name', type: 'string' }
+		,{ name: 'defaultValue', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'projectItem', type: 'presence' }
+		,{ field: 'type', type: 'presence' }
+		,{ field: 'name', type: 'presence' }
+		,{ field: 'defaultValue', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
