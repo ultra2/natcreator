a007279d859da4c3eb07351c5b6832eb736d7254
ultra2 <user10000@freemail.hu>
2012-10-23 12:44:18 -0700

    commit

---
+++ designer/models/PPProjectItem.js
@@ -0,0 +1,14 @@
+Ext.define('designer.model.PPProjectItem', { 
+	extend: 'designer.model.PersistentProject',
+	alias: 'PPProjectItem'
+	
+	,fields: [
+		{ name: 'userConfig', type: 'auto', debug: true }
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
