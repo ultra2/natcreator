d530299d5cc8bd1ae6a4c78d693a064c398881e6
ultra2 <user10000@freemail.hu>
2012-10-23 12:49:09 -0700

    commit

---
+++ designer/models/RPGetTemplateProjectGithubZipUrl.js
@@ -0,0 +1,13 @@
+Ext.define('designer.model.RPGetTemplateProjectGithubZipUrl', { 
+	extend: 'designer.model.RequestPost',
+	alias: 'RPGetTemplateProjectGithubZipUrl'
+	
+	,fields: [
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
