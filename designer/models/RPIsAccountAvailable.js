f73edfe140ca4f3ebb15f59c6e34d111cdf8af27
ultra2 <user10000@freemail.hu>
2012-10-26 14:07:43 -0700

    commit

---
+++ designer/models/RPIsAccountAvailable.js
@@ -0,0 +1,15 @@
+Ext.define('designer.model.RPIsAccountAvailable', { 
+	extend: 'designer.model.RequestPost',
+	alias: 'RPIsAccountAvailable'
+	
+	,fields: [
+		{ name: 'username', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'username', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
