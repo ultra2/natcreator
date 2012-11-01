75f32431f31ce9ceb5548fd4fb88bad7fa79c3c4
ultra2 <user10000@freemail.hu>
2012-11-01 13:54:52 -0700

    commit

--- designer/ExtjsModelRequires.js
+++ designer/ExtjsModelRequires.js
@@ -37,7 +37,7 @@ Ext.define('designer.ExtjsModelRequires', {
 		Ext.syncRequire('designer.model.CDPropertyNode');
 		Ext.syncRequire('designer.model.CDClassConfig');
 		Ext.syncRequire('designer.model.CDClass');
-		Ext.syncRequire('designer.model.CDClassMetaNode');
+		Ext.syncRequire('designer.model.CDViewNode');
 		Ext.syncRequire('designer.model.CDDataType');
 		Ext.syncRequire('designer.model.ClientWeb');
 		Ext.syncRequire('designer.model.RequestGet');
