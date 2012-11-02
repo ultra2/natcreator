bad143035016f303ed197a1bd8591eb7b6aa0fab
ultra2 <user10000@freemail.hu>
2012-11-02 07:26:34 -0700

    commit

---
+++ designer/models/CDInspectorNode.js
@@ -0,0 +1,15 @@
+Ext.define('designer.model.CDInspectorNode', { 
+	extend: 'designer.model.ClientDesigner',
+	alias: 'CDInspectorNode'
+	
+	,fields: [
+		{ name: 'name', type: 'string', debug: true }
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
+Ext.data.NodeInterface.decorate(Ext.create('designer.model.CDInspectorNode'));
