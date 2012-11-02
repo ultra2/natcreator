df7daa7ffba070097956939fd1fbde7e6db63de5
ultra2 <user10000@freemail.hu>
2012-11-02 07:33:47 -0700

    commit

---
+++ designer/models/CDInspectorNode.js
@@ -0,0 +1,14 @@
+Ext.define('designer.model.CDInspectorNode', { 
+	extend: 'designer.model.ClientDesigner',
+	alias: 'CDInspectorNode'
+	
+	,fields: [
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
+Ext.data.NodeInterface.decorate(Ext.create('designer.model.CDInspectorNode'));
