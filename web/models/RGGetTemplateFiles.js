f1c33a7504e7ea92d71c41b369fb3eceea13ac8e
ultra2 <user10000@freemail.hu>
2012-11-06 10:03:50 -0800

    commit

---
+++ web/models/RGGetTemplateFiles.js
@@ -0,0 +1,13 @@
+Ext.define('web.model.RGGetTemplateFiles', { 
+	extend: 'web.model.RequestGet',
+	alias: 'RGGetTemplateFiles'
+	
+	,fields: [
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
