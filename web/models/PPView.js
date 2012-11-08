a6ba728bfed23bc74c289205f284d317de23eb21
ultra2 <user10000@freemail.hu>
2012-11-08 09:32:37 -0800

    commit

---
+++ web/models/PPView.js
@@ -0,0 +1,17 @@
+Ext.define('web.model.PPView', { 
+	extend: 'web.model.PPProjectItem',
+	alias: 'PPView'
+	
+	,fields: [
+		{ name: 'name', type: 'string' }
+		,{ name: 'alias', type: 'string' }
+		,{ name: 'meta', type: 'auto' }
+		,{ name: 'config', type: 'auto', persist: false }
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
