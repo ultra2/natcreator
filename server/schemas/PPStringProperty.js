8cbc45e9e4ffb0e6ee9eeebd56d0995a970b52a8
ultra2 <user10000@freemail.hu>
2012-11-08 18:14:38 -0800

    commit

---
+++ server/schemas/PPStringProperty.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose')
+   ,Schema = mongoose.Schema
+   ,commonPlugin = require('../schemaPlugins/common.js');
+
+exports.PPStringProperty = new Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,created: { type: Date }
+		,modified: { type: Date }
+		,version: { type: Number }
+		,deleted: { type: Boolean }
+		,userConfig: { type: mongoose.Schema.Types.Mixed }
+		,isKey: { type: Boolean }
+		,isNullable: { type: Boolean }
+		,isArray: { type: Boolean }
+		,name: { type: String }
+		,defaultValue: { type: String }
+		,minLength: { type: Number }
+		,vFormat: { type: String }
+		,maxLength: { type: Number }
+});
+exports.PPStringProperty.plugin(commonPlugin);
