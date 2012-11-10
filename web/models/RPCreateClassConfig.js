cf531768ff3e12e4eb25de6f51ae6b695dcedefd
ultra2 <user10000@freemail.hu>
2012-11-10 15:09:56 -0800

    commit

---
+++ web/models/RPCreateClassConfig.js
@@ -0,0 +1,18 @@
+Ext.define('web.model.RPCreateClassConfig', { 
+	extend: 'web.model.RequestPost',
+	alias: 'RPCreateClassConfig'
+	
+	,fields: [
+		{ name: 'classId', type: 'string', debug: true }
+		,{ name: 'name', type: 'string' }
+		,{ name: 'types', type: 'auto', debug: true }
+		,{ name: 'values', type: 'auto', debug: true }
+		,{ name: 'defaultValue', type: 'auto', debug: true }
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
