db7772ccc4189732f956f5c448326bab0607aad4
ultra2 <user10000@freemail.hu>
2012-11-10 14:00:31 -0800

    commit

---
+++ designer/models/RGSetupGithubCallback.js
@@ -0,0 +1,15 @@
+Ext.define('designer.model.RGSetupGithubCallback', { 
+	extend: 'designer.model.RequestGet',
+	alias: 'RGSetupGithubCallback'
+	
+	,fields: [
+		{ name: 'code', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'code', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
