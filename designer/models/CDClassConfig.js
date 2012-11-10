9ad826d78b3dccd890cdf0a84efef1a3071b12d3
ultra2 <user10000@freemail.hu>
2012-11-10 12:30:31 -0800

    commit

---
+++ designer/models/CDClassConfig.js
@@ -0,0 +1,17 @@
+Ext.define('designer.model.CDClassConfig', { 
+	extend: 'designer.model.ClientDesigner',
+	alias: 'CDClassConfig'
+	
+	,fields: [
+		{ name: 'name', type: 'string', debug: true }
+		,{ name: 'types', type: 'auto' }
+		,{ name: 'values', type: 'auto' }
+		,{ name: 'defaultValue', type: 'auto' }
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
---
+++ designer/models/PPProjectConfig.js
@@ -0,0 +1,14 @@
+Ext.define('designer.model.PPProjectConfig', { 
+	extend: 'designer.model.PersistentProject',
+	alias: 'PPProjectConfig'
+	
+	,fields: [
+		{ name: 'toolbox', type: 'auto', debug: true }
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
