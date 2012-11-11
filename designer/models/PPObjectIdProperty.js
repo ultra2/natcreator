16bfa4e0a30c5d4cd62723efcc6e032e244271c8
ultra2 <user10000@freemail.hu>
2012-11-11 15:18:16 -0800

    commit

---
+++ designer/models/PPObjectIdProperty.js
@@ -0,0 +1,13 @@
+Ext.define('designer.model.PPObjectIdProperty', { 
+	extend: 'designer.model.PPDataProperty',
+	alias: 'PPObjectIdProperty'
+	
+	,fields: [
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
---
+++ designer/models/RPIsAuthenticated.js
@@ -0,0 +1,13 @@
+Ext.define('designer.model.RPIsAuthenticated', { 
+	extend: 'designer.model.RequestPost',
+	alias: 'RPIsAuthenticated'
+	
+	,fields: [
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
