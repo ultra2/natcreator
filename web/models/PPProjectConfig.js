5bfe4a8a9da46b76cc8f244864537d32f77caf87
ultra2 <user10000@freemail.hu>
2012-11-11 09:32:53 -0800

    commit

---
+++ web/models/PPProjectConfig.js
@@ -0,0 +1,14 @@
+Ext.define('web.model.PPProjectConfig', { 
+	extend: 'web.model.PersistentProject',
+	alias: 'PPProjectConfig'
+	
+	,fields: [
+		{ name: 'toolbox', type: 'auto', debug: true }
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+	,fieldInfos: [
+	]
+});
