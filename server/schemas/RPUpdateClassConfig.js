749bbc7723a94ab9262682b297b5125635fae4fd
ultra2 <user10000@freemail.hu>
2012-11-12 09:10:33 -0800

    commit

---
+++ server/schemas/RPUpdateClassConfig.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose')
+   ,Schema = mongoose.Schema
+   ,commonPlugin = require('../schemaPlugins/common.js');
+
+exports.RPUpdateClassConfig = new Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,classId: { type: String }
+		,name: { type: String }
+		,newName: { type: String }
+		,newValues: { type: mongoose.Schema.Types.Mixed }
+		,newDefaultValue: { type: mongoose.Schema.Types.Mixed }
+		,newAccepted: { type: mongoose.Schema.Types.Mixed }
+		,newNotAccepted: { type: mongoose.Schema.Types.Mixed }
+});
+exports.RPUpdateClassConfig.plugin(commonPlugin);
