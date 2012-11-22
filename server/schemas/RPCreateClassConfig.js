330fad105a29641e51eb5b107f4fccced177c696
ultra2 <user10000@freemail.hu>
2012-11-21 17:27:32 -0800

    commit

---
+++ server/schemas/RPCreateClassConfig.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose')
+   ,Schema = mongoose.Schema
+   ,commonPlugin = require('../schemaPlugins/common.js');
+
+exports.RPCreateClassConfig = new Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,classId: { type: String }
+		,name: { type: String }
+		,types: { type: mongoose.Schema.Types.Mixed }
+		,values: { type: mongoose.Schema.Types.Mixed }
+		,defaultValue: { type: mongoose.Schema.Types.Mixed }
+		,accepted: { type: mongoose.Schema.Types.Mixed }
+		,notAccepted: { type: mongoose.Schema.Types.Mixed }
+});
+exports.RPCreateClassConfig.plugin(commonPlugin);
