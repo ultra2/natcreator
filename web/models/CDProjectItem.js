ce68a641acc820d8d31d6e817ffd696987a9e3b8
ultra2 <user10000@freemail.hu>
2012-11-26 21:02:08 -0800

    commit

---
+++ web/models/CDProjectItem.js
@@ -0,0 +1,15 @@
+Ext.define('web.model.CDProjectItem', { 
+	extend: 'web.model.ClientDesigner',
+	alias: 'CDProjectItem'
+	
+	,fields: [
+		{ name: 'name', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'name', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
