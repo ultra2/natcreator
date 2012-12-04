de1bf48e56c6853a2442346bfcb2f562301d4a37
ultra2 <user10000@freemail.hu>
2012-12-04 00:13:06 -0800

    commit

---
+++ designer/models/RPCreateClassConfig.js
@@ -0,0 +1,22 @@
+Ext.define('designer.model.RPCreateClassConfig', { 
+	extend: 'designer.model.RequestPost',
+	alias: 'RPCreateClassConfig'
+	
+	,fields: [
+		{ name: 'classId', type: 'string', debug: true }
+		,{ name: 'name', type: 'string' }
+		,{ name: 'types', type: 'auto', debug: true }
+		,{ name: 'values', type: 'auto', debug: true }
+		,{ name: 'defaultValue', type: 'auto', debug: true }
+		,{ name: 'accepted', type: 'auto', debug: true }
+		,{ name: 'notAccepted', type: 'auto', debug: true }
+		,{ name: 'isDependency', type: 'boolean', debug: true }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'name', type: 'presence' }
+	]
+	,fieldInfos: [
+	]
+});
