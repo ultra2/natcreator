610d349e6520e877c4f9b170a782f5ac6149f843
ultra2 <user10000@freemail.hu>
2012-12-06 08:16:55 -0800

    commit

---
+++ web/models/RPUpdateClassConfig.js
@@ -0,0 +1,19 @@
+Ext.define('web.model.RPUpdateClassConfig', { 
+	extend: 'web.model.RequestPost',
+	alias: 'RPUpdateClassConfig'
+	
+	,fields: [
+		{ name: 'classId', type: 'string' }
+		,{ name: 'name', type: 'string' }
+		,{ name: 'newName', type: 'string' }
+		,{ name: 'newValues', type: 'auto' }
+		,{ name: 'newDefaultValue', type: 'auto' }
+		,{ name: 'newAccepted', type: 'auto' }
+		,{ name: 'newNotAccepted', type: 'auto' }
+		,{ name: 'newIsDependency', type: 'boolean' }
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+});
