48e98725b2aa9b987037dc2b5358b1fbdeb1a429
ultra2 <user10000@freemail.hu>
2012-12-06 03:53:30 -0800

    commit

---
+++ designer/models/CDInspectorNode.js
@@ -0,0 +1,13 @@
+Ext.define('designer.model.CDInspectorNode', { 
+	extend: 'designer.model.ClientDesigner',
+	alias: 'CDInspectorNode'
+	
+	,fields: [
+		{ name: 'name', type: 'string' }
+		,{ name: 'referenceType', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+});
---
+++ designer/models/RPCreateClassConfig.js
@@ -0,0 +1,20 @@
+Ext.define('designer.model.RPCreateClassConfig', { 
+	extend: 'designer.model.RequestPost',
+	alias: 'RPCreateClassConfig'
+	
+	,fields: [
+		{ name: 'classId', type: 'string' }
+		,{ name: 'name', type: 'string' }
+		,{ name: 'types', type: 'auto' }
+		,{ name: 'values', type: 'auto' }
+		,{ name: 'defaultValue', type: 'auto' }
+		,{ name: 'accepted', type: 'auto' }
+		,{ name: 'notAccepted', type: 'auto' }
+		,{ name: 'isDependency', type: 'boolean' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'name', type: 'presence' }
+	]
+});
