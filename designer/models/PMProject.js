c5e7f42a4c6e8d74e19f0570df09aa3121a41779
ultra2 <user10000@freemail.hu>
2012-12-10 12:13:32 -0800

    commit

---
+++ designer/models/PMProject.js
@@ -0,0 +1,23 @@
+Ext.define('designer.model.PMProject', { 
+	extend: 'designer.model.PersistentMaster',
+	alias: 'PMProject'
+	
+	,fields: [
+		{ name: 'name', type: 'string' }
+		,{ name: 'description', type: 'string' }
+		,{ name: 'isTemplate', type: 'boolean' }
+		,{ name: 'template_id', type: 'string' }
+		,{ name: 'guide', type: 'string' }
+		,{ name: 'connectionUrl', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'name', type: 'presence' }
+		,{ field: 'name', type: 'length', min: 6, max: 30 }
+		,{ field: 'description', type: 'presence' }
+		,{ field: 'description', type: 'length', min: 0, max: 500 }
+		,{ field: 'template_id', type: 'presence' }
+		,{ field: 'connectionUrl', type: 'presence' }
+	]
+});
