c3743196a166a18cebec8cd99eab69a9ae9510aa
ultra2 <user10000@freemail.hu>
2012-12-10 12:30:19 -0800

    commit

--- server/schemas/RPCloseProject.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose')
-   ,Schema = mongoose.Schema
-   ,commonPlugin = require('../schemaPlugins/common.js');
-
-exports.RPCloseProject = Schema({ 
-		_id: { type: String }
-		,_type: { type: String }
-});
-exports.RPCloseProject.plugin(commonPlugin);
