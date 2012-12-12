4828a18095144af356e7f4d4acdd8bc0a6c5efe8
ultra2 <user10000@freemail.hu>
2012-12-12 04:42:19 -0800

    commit

---
+++ server/schemas/QueryResult.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose')
+   ,Schema = mongoose.Schema
+   ,commonPlugin = require('../schemaPlugins/common.js');
+
+exports.QueryResult = new Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,created: { type: Date, default: new Date(0) }
+		,modified: { type: Date, default: new Date(0) }
+		,deleted: { type: Boolean }
+		,version: { type: Number }
+});
+exports.QueryResult.plugin(commonPlugin);
