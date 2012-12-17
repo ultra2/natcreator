a52ec41ab8398309db0a7d81cfa629b9bbff8adb
ultra2 <user10000@freemail.hu>
2012-12-17 14:41:12 -0800

    commit

---
+++ web/models/QRLookup.js
@@ -0,0 +1,13 @@
+Ext.define('web.model.QRLookup', { 
+	extend: 'web.model.QueryResult',
+	alias: 'QRLookup'
+	
+	,fields: [
+		{ name: 'name', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'name', type: 'presence' }
+	]
+});
