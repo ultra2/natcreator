fa5e4853c8179351d91b2af9af5be53a35f52d60
ultra2 <user10000@freemail.hu>
2012-12-17 14:19:47 -0800

    commit

--- designer/view/generated/SendQuestionWindow.js
+++ designer/view/generated/SendQuestionWindow.js
@@ -1,6 +1,13 @@
 Ext.define('designer.view.generated.SendQuestionWindow', {
 	extend: 'NAT.window.Window',
 	alias: 'widget.SendQuestionWindow',
+	height: 500,
+	width: 600,
+	layout: {
+		type: 'fit'
+	},
+	title: 'Request feature / Ask Question',
+	modal: true,
 	initComponent: function(){
 		var me = this;
 		Ext.applyIf(me, {
