c64a169655f8983066fab0f5c6f3ed01dcd28ea4
ultra2 <user10000@freemail.hu>
2012-12-17 11:25:33 -0800

    commit

---
+++ designer/models/RPRenameGenerator.js
@@ -0,0 +1,16 @@
+Ext.define('designer.model.RPRenameGenerator', { 
+	extend: 'designer.model.RequestPost',
+	alias: 'RPRenameGenerator'
+	
+	,fields: [
+		{ name: 'name', type: 'string' }
+		,{ name: 'generatorId', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'name', type: 'presence' }
+		,{ field: 'name', type: 'format', matcher: /^([\w\-\.])+$/ }
+		,{ field: 'generatorId', type: 'presence' }
+	]
+});
