b4a3c89ac189a0ff84eb8fd004d4d4d4465d5056
ultra2 <user10000@freemail.hu>
2012-12-21 10:33:31 -0800

    commit

---
+++ designer/models/PMSharedGenerator.js
@@ -0,0 +1,19 @@
+Ext.define('designer.model.PMSharedGenerator', { 
+	extend: 'designer.model.PersistentMaster',
+	alias: 'PMSharedGenerator'
+	
+	,fields: [
+		{ name: 'name', type: 'string' }
+		,{ name: 'originProject_id', type: 'string' }
+		,{ name: 'originGenerator_id', type: 'string' }
+		,{ name: 'generatorVersion', type: 'string', defaultValue: '1.0' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'name', type: 'presence' }
+		,{ field: 'originProject_id', type: 'presence' }
+		,{ field: 'originGenerator_id', type: 'presence' }
+		,{ field: 'generatorVersion', type: 'presence' }
+	]
+});
