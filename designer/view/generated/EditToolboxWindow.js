903ed6bb3915d03602726fa62d43ba70411c3b49
ultra2 <user10000@freemail.hu>
2012-12-22 10:17:49 -0800

    commit

---
+++ designer/view/generated/EditToolboxWindow.js
@@ -0,0 +1,154 @@
+Ext.define('designer.view.generated.EditToolboxWindow', {
+	extend: 'NAT.window.Window',
+	alias: 'widget.EditToolboxWindow',
+	height: 500,
+	width: 800,
+	layout: {
+		type: 'hbox',
+		align: 'stretch'
+	},
+	closable: false,
+	title: 'Edit Toolbox',
+	modal: true,
+	initComponent: function(){
+		var me = this;
+		Ext.applyIf(me, {
+			items: [
+				{
+					xtype: 'nattree',
+					itemId: 'treeAvailableClasses',
+					flex: 1,
+					padding: '2',
+					title: 'Available Classes',
+					hideHeaders: true,
+					forceFit: true,
+					viewConfig: {
+						getRowClass: function(record) {        if (!record) return '';        return record.get('cls');    }
+					},
+					columns: [
+						{
+							xtype: 'treecolumn',
+							dataIndex: 'name'
+						}
+					]
+				},
+				{
+					xtype: 'container',
+					cls: 'commandbar',
+					defaults: {
+						margins: '2.5f 5 2.5f 5'
+					},
+					layout: {
+						type: 'vbox',
+						align: 'center',
+						pack: 'center'
+					},
+					items: [
+						{
+							xtype: 'button',
+							itemId: 'btnAddClass',
+							icon: 'https://extsolutions.herokuapp.com/graphics/Momentum_MatteEntireSet1/16/right1.png',
+							tooltip: 'Add class to toolbox group'
+						},
+						{
+							xtype: 'button',
+							itemId: 'btnRemoveClass',
+							icon: 'https://extsolutions.herokuapp.com/graphics/Momentum_MatteEntireSet1/16/left.png',
+							tooltip: 'Remove class from toolbox group'
+						}
+					]
+				},
+				{
+					xtype: 'natgrid',
+					title: 'Selected Classes',
+					padding: '2',
+					itemId: 'gridSelectedClasses',
+					hideHeaders: true,
+					forceFit: true,
+					flex: 1,
+					columns: [
+						{
+							xtype: 'gridcolumn',
+							dataIndex: 'name'
+						}
+					],
+					viewConfig: {
+						xtype: 'gridview',
+						plugins: [
+							{
+								xtype: 'gridviewdragdrop',
+								dragGroup: 'EditToolboxDD',
+								dropGroup: 'EditToolboxDD'
+							}
+						]
+					}
+				}
+			],
+			dockedItems: [
+				{
+					xtype: 'container',
+					dock: 'top',
+					cls: 'commandbar',
+					defaults: {
+						margins: 5
+					},
+					layout: {
+						type: 'hbox',
+						align: 'middle'
+					},
+					items: [
+						{
+							xtype: 'combobox',
+							itemId: 'cbGroup',
+							width: 254,
+							fieldLabel: 'Group',
+							labelWidth: 40
+						},
+						{
+							xtype: 'button',
+							itemId: 'btnNewGroup',
+							icon: 'https://extsolutions.herokuapp.com/graphics/Momentum_MatteEntireSet1/16/add.png',
+							tooltip: 'Create new toolbox group'
+						},
+						{
+							xtype: 'button',
+							itemId: 'btnDeleteGroup',
+							icon: 'https://extsolutions.herokuapp.com/graphics/Momentum_MatteEntireSet1/16/delete.png',
+							tooltip: 'Delete a toolbox group'
+						}
+					]
+				},
+				{
+					xtype: 'container',
+					dock: 'bottom',
+					flex: 1,
+					defaults: {
+						margins: '0 0 0 5'
+					},
+					layout: {
+						type: 'hbox',
+						align: 'middle',
+						pack: 'end',
+						padding: '5'
+					},
+					items: [
+						{
+							xtype: 'button',
+							itemId: 'btnSave',
+							minWidth: 100,
+							ui: 'green',
+							text: 'Save'
+						},
+						{
+							xtype: 'button',
+							itemId: 'btnCancel',
+							minWidth: 100,
+							text: 'Cancel'
+						}
+					]
+				}
+			]
+		});
+		me.callParent(arguments);
+	}
+});
