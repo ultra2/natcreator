0a49465e01388a7d87e0aa107b9d17e4bb5e26bc
ultra2 <user10000@freemail.hu>
2012-12-30 14:49:44 -0800

    commit

---
+++ designer/models/CDClassMetaNode.js
@@ -0,0 +1,17 @@
+Ext.define('designer.model.CDClassMetaNode', { 
+	extend: 'designer.model.ClientDesigner',
+	alias: 'CDClassMetaNode'
+	
+	,fields: [
+		{ name: 'type', type: 'string' }
+		,{ name: 'reference', type: 'auto' }
+		,{ name: 'codeClass', type: 'auto' }
+		,{ name: 'userConfig', type: 'auto' }
+		,{ name: 'customConfigs', type: 'auto' }
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+});
+Ext.data.NodeInterface.decorate(Ext.create('designer.model.CDClassMetaNode'));
---
+++ designer/models/QRSharedGeneratorList.js
@@ -0,0 +1,15 @@
+Ext.define('designer.model.QRSharedGeneratorList', { 
+	extend: 'designer.model.QueryResult',
+	alias: 'QRSharedGeneratorList'
+	
+	,fields: [
+		{ name: 'name', type: 'string' }
+		,{ name: 'generatorVersion', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'name', type: 'presence' }
+		,{ field: 'generatorVersion', type: 'presence' }
+	]
+});
