127d918727a7b9de6c9c704549e5575095dd9f74
ultra2 <user10000@freemail.hu>
2012-12-30 19:54:39 -0800

    commit

---
+++ designer/models/RPIsGeneratorUpdatable.js
@@ -0,0 +1,13 @@
+Ext.define('designer.model.RPIsGeneratorUpdatable', { 
+	extend: 'designer.model.RequestPost',
+	alias: 'RPIsGeneratorUpdatable'
+	
+	,fields: [
+		{ name: 'generatorId', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'generatorId', type: 'presence' }
+	]
+});
