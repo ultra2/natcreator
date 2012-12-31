376f0bc90fcc01463a507aa96467f56f14753e92
ultra2 <user10000@freemail.hu>
2012-12-30 21:40:55 -0800

    commit

---
+++ web/models/PPProjectSetting.js
@@ -0,0 +1,19 @@
+Ext.define('web.model.PPProjectSetting', { 
+	extend: 'web.model.PersistentProject',
+	alias: 'PPProjectSetting'
+	
+	,fields: [
+		{ name: 'generationTarget', type: 'string', defaultValue: 'zip' }
+		,{ name: 'githubUser', type: 'string' }
+		,{ name: 'githubRepository', type: 'string' }
+		,{ name: 'codeGenerationWindowCloseOnSuccessful', type: 'boolean' }
+		,{ name: 'includeTemplateFilesIntoGeneration', type: 'boolean', defaultValue: 'true' }
+		,{ name: 'fontSize', type: 'int', defaultValue: '18' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'generationTarget', type: 'presence' }
+		,{ field: 'fontSize', type: 'presence' }
+	]
+});
