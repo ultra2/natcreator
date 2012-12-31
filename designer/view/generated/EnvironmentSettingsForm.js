ef4028f3dcd3fa86d3a272892ac39e85cfb3b9c6
ultra2 <user10000@freemail.hu>
2012-12-30 21:41:22 -0800

    commit

--- designer/view/generated/EnvironmentSettingsForm.js
+++ designer/view/generated/EnvironmentSettingsForm.js
@@ -26,7 +26,25 @@ Ext.define('designer.view.generated.EnvironmentSettingsForm', {
 							labelWidth: 80,
 							width: 160,
 							propertyPath: 'fontSize',
-							itemId: 'cbFontSize'
+							itemId: 'cbFontSize',
+							comboData: [
+								'8',
+								'9',
+								'10',
+								'11',
+								'12',
+								'14',
+								'16',
+								'18',
+								'20',
+								'22',
+								'24',
+								'26',
+								'28',
+								'36',
+								'42',
+								'72'
+							]
 						}
 					]
 				},
