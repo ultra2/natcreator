3f5289f22bb99f9fe83051083c40906cb3428d95
ultra2 <user10000@freemail.hu>
2012-12-31 09:36:42 -0800

    commit

---
+++ designer/models/RPUpdateGenerator.js
@@ -0,0 +1,13 @@
+Ext.define('designer.model.RPUpdateGenerator', { 
+	extend: 'designer.model.RequestPost',
+	alias: 'RPUpdateGenerator'
+	
+	,fields: [
+		{ name: 'generatorId', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'generatorId', type: 'presence' }
+	]
+});
