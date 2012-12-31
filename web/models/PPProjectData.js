fc05a9bebefd5620b89d9a22531facd148579276
ultra2 <user10000@freemail.hu>
2012-12-30 20:31:17 -0800

    commit

---
+++ web/models/PPProjectData.js
@@ -0,0 +1,13 @@
+Ext.define('web.model.PPProjectData', { 
+	extend: 'web.model.PersistentProject',
+	alias: 'PPProjectData'
+	
+	,fields: [
+		{ name: 'githubToken', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'githubToken', type: 'presence' }
+	]
+});
