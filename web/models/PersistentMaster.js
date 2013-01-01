89d4a7fa08c7759c6f5ce7f048767c7b1ff586f8
ultra2 <user10000@freemail.hu>
2013-01-01 15:36:41 -0800

    commit

---
+++ web/models/PersistentMaster.js
@@ -0,0 +1,15 @@
+Ext.define('web.model.PersistentMaster', { 
+	extend: 'web.model.CRBaseObject',
+	alias: 'PersistentMaster'
+	
+	,fields: [
+		{ name: 'created', type: 'date' }
+		,{ name: 'modified', type: 'date' }
+		,{ name: 'version', type: 'int' }
+		,{ name: 'deleted', type: 'boolean' }
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+});
