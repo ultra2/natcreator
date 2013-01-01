3c613ae356ceac1c4c366f70d447f177555d4154
ultra2 <user10000@freemail.hu>
2013-01-01 15:37:07 -0800

    commit

---
+++ designer/view/generated/PropertyTree.js
@@ -0,0 +1,10 @@
+Ext.define('designer.view.generated.PropertyTree', {
+	extend: 'NAT.tree.Panel',
+	alias: 'widget.PropertyTree',
+	initComponent: function(){
+		var me = this;
+		Ext.applyIf(me, {
+		});
+		me.callParent(arguments);
+	}
+});
