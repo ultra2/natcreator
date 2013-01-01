219490b76dcd8c49d7388be7df37df81285470ed
ultra2 <user10000@freemail.hu>
2013-01-01 15:52:17 -0800

    commit

--- designer/view/generated/PropertyTree.js
+++ designer/view/generated/PropertyTree.js
@@ -1,6 +1,13 @@
 Ext.define('designer.view.generated.PropertyTree', {
 	extend: 'NAT.tree.Panel',
 	alias: 'widget.PropertyTree',
+	bodyStyle: 'background:#EDEBE0;',
+	enableColumnMove: false,
+	columnLines: true,
+	rootVisible: false,
+	viewConfig: {
+		getRowClass: function(record) {        if (!record) return '';        return record.get('cls');    }
+	},
 	initComponent: function(){
 		var me = this;
 		Ext.applyIf(me, {
