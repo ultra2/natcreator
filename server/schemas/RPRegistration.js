27705ca49cf8c81e709337fa79ff1d230cf2c353
ultra2 <user10000@freemail.hu>
2013-01-02 17:31:43 -0800

    commit

---
+++ server/schemas/RPRegistration.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose')
+   ,Schema = mongoose.Schema
+   ,commonPlugin = require('../schemaPlugins/common.js');
+
+exports.RPRegistration = new Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,username: { type: String }
+		,email: { type: String }
+		,password: { type: String }
+		,confirmPassword: { type: String }
+});
+exports.RPRegistration.plugin(commonPlugin);
