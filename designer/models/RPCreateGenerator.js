45c9be886d8b6b33abdb50a8f020f9e867a4adbc
ultra2 <user10000@freemail.hu>
2013-01-07 20:48:26 -0800

    commit

---
+++ designer/models/RPCreateGenerator.js
@@ -0,0 +1,14 @@
+Ext.define('designer.model.RPCreateGenerator', { 
+	extend: 'designer.model.RequestPost',
+	alias: 'RPCreateGenerator'
+	
+	,fields: [
+		{ name: 'name', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'name', type: 'presence' }
+		,{ field: 'name', type: 'format', matcher: /^([\w\-\.])+$/ }
+	]
+});
