f05575ebab87b01dae95eb3bd7686edf5e34e00c
ultra2 <user10000@freemail.hu>
2013-01-07 22:32:41 -0800

    commit

---
+++ web/models/PPProjectConfig.js
@@ -0,0 +1,15 @@
+Ext.define('web.model.PPProjectConfig', { 
+	extend: 'web.model.PersistentProject',
+	alias: 'PPProjectConfig'
+	
+	,fields: [
+		{ name: 'toolbox', type: 'auto' }
+		,{ name: 'solutionExplorer', type: 'auto' }
+	]
+	,associations: [
+		{ type: 'hasMany', name: 'generationSteps', associationKey: 'generationSteps', model: 'web.model.PPGenerationStep' , primaryKey: '_id' }
+	]
+	,validations: [
+		{ field: 'generationSteps_id', type: 'presence' }
+	]
+});
