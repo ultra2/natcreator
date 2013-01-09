5f470d13df88de95c9db117029d223f7fef6bcd2
ultra2 <user10000@freemail.hu>
2013-01-08 19:31:33 -0800

    commit

---
+++ web/models/PMAccountProjectRole.js
@@ -0,0 +1,13 @@
+Ext.define('web.model.PMAccountProjectRole', { 
+	extend: 'web.model.PersistentMaster',
+	alias: 'PMAccountProjectRole'
+	
+	,fields: [
+		{ name: 'name', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'name', type: 'presence' }
+	]
+});
