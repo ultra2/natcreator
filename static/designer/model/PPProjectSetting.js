82f7f4d3d669ddc21e9e5f706705a56ea46eef96
ultra2 <user10000@freemail.hu>
2013-02-03 09:27:58 -0800

    commit

---
+++ static/designer/model/PPProjectSetting.js
@@ -0,0 +1,20 @@
+Ext.define('designer.model.PPProjectSetting', { 
+	extend: 'designer.model.PersistentProject',
+	alias: 'PPProjectSetting'
+	
+	,fields: [
+		{ name: 'generationTarget', type: 'string', defaultValue: 'zip' }
+		,{ name: 'githubRepository', type: 'string' }
+		,{ name: 'codeGenerationWindowCloseOnSuccessful', type: 'boolean' }
+		,{ name: 'includeTemplateFilesIntoGeneration', type: 'boolean', defaultValue: 'true' }
+		,{ name: 'fontSize', type: 'int', defaultValue: '18' }
+		,{ name: 'githubTemplateRepository', type: 'string' }
+		,{ name: 'herokuApp', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'generationTarget', type: 'presence' }
+		,{ field: 'fontSize', type: 'presence' }
+	]
+});
