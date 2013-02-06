5dc13907118ffac1013db7ec6becd95af75f5b81
ultra2 <user10000@freemail.hu>
2013-02-06 08:16:51 -0800

    commit

--- schemas/RPResetProjectConfig.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose')
-   ,Schema = mongoose.Schema
-   ,commonPlugin = require('../schemaPlugins/common.js');
-
-exports.RPResetProjectConfig = new Schema({ 
-		_id: { type: String }
-		,_type: { type: String }
-});
-exports.RPResetProjectConfig.plugin(commonPlugin);
