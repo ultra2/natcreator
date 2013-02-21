dc48efcfe6fd1672d238de4cb33d0ed2dd7ef1b3
ultra2 <user10000@freemail.hu>
2013-02-21 03:05:12 -0800

    commit

---
+++ static/designer/view/generated/LoginWindow.js
@@ -0,0 +1,93 @@
+Ext.define('designer.view.generated.LoginWindow', { 
+	extend: 'NAT.window.Window',
+	alias: 'widget.LoginWindow',
+	
+	height: 220,
+	width: 446,
+	resizable: false,
+	title: 'Log In',
+	modal: true,
+	layout: {
+		type: 'fit'		
+	},
+	
+	initComponent: function(){
+		var me = this;
+		Ext.applyIf(me, {
+			dockedItems: [
+				{
+					xtype: 'container',					
+					cls: 'commandpanel',					
+					defaults: {					
+						margins: '0 0 0 5',					
+						autoWidth: true					
+					},					
+					dock: 'bottom',					
+					height: 30,					
+					layout: {
+						type: 'hbox',						
+						align: 'middle',						
+						pack: 'end',						
+						padding: '5'						
+					},					
+					items: [
+						{
+							xtype: 'natbutton',							
+							itemId: 'btnResetPasswordRequest',							
+							text: 'Can\'t access your account?',							
+							ui: 'blue'							
+						},						
+						{
+							xtype: 'natbutton',							
+							itemId: 'btnRegister',							
+							minWidth: 100,							
+							text: 'Register',							
+							ui: 'green'							
+						},						
+						{
+							xtype: 'natbutton',							
+							itemId: 'btnLogin',							
+							minWidth: 100,							
+							text: 'Log In',							
+							ui: 'green'							
+						}						
+					]					
+				}				
+			],			
+			items: [
+				{
+					xtype: 'natform',					
+					bodyPadding: 10,					
+					defaults: {					
+						anchor: '100%',					
+						labelWidth: 120					
+					},					
+					itemId: 'edLogin',					
+					items: [
+						{
+							xtype: 'nattextfield',							
+							fieldLabel: 'Username',							
+							itemId: 'txtUsername',							
+							labelAlign: 'top',							
+							propertyPath: 'username'							
+						},						
+						{
+							xtype: 'nattextfield',							
+							fieldLabel: 'Password',							
+							inputType: 'password',							
+							itemId: 'txtPassword',							
+							labelAlign: 'top',							
+							propertyPath: 'password'							
+						},						
+						{
+							xtype: 'natcheckboxfield',							
+							fieldLabel: 'Remember me',							
+							propertyPath: 'rememberMe'							
+						}						
+					]					
+				}				
+			]			
+		});
+		me.callParent(arguments);
+	}
+});
