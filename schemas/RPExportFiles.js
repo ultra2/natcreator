24a02e229cfb36dd502bcd53d477e28bb90ebbdc
ultra2 <user10000@freemail.hu>
2013-03-01 20:55:29 -0800

    commit

---
+++ schemas/RPExportFiles.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose')
+   ,Schema = mongoose.Schema
+   ,commonPlugin = require('../schemaPlugins/common.js');
+var ROFile = require('../schemas/ROFile.js').ROFile;
+
+exports.RPExportFiles = new Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,path: { type: String }
+		,deleteDirectory: { type: Boolean }
+		,files: [ROFile]
+});
+exports.RPExportFiles.plugin(commonPlugin);
