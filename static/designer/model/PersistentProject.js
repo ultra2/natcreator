a412e7432c49cf16385c94781695b9ecee9d1b6c
ultra2 <user10000@freemail.hu>
2013-03-06 07:35:47 -0800

    commit

--- static/designer/model/PersistentProject.js
+++
@@ -1,4 +0,0 @@
-Ext.define('designer.model.PersistentProject', { 
-	extend: 'NAT.data.PersistentModel'
-	
-});
--- static/designer/model/RPGetGithubInfo.js
+++
@@ -1,4 +0,0 @@
-Ext.define('designer.model.RPGetGithubInfo', { 
-	extend: 'designer.model.RequestPost'
-	
-});
