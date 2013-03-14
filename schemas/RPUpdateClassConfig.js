c542fb47b894f93552fee26f8135bcf9d6bd1309
ultra2 <user10000@freemail.hu>
2013-03-14 11:00:44 -0700

    commit

---
+++ schemas/RPUpdateClassConfig.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    async = require('async'),
+    db = require('../modules/db.js');
+
+var RPUpdateClassConfig = new Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,classId: { type: String }
+		,name: { type: String }
+		,newName: { type: String }
+		,newValues: { type: mongoose.Schema.Types.Mixed }
+		,newDefaultValue: { type: mongoose.Schema.Types.Mixed }
+		,newNotAccepted: { type: mongoose.Schema.Types.Mixed }
+		,newDependency: { type: Boolean }
+		,newHidden: { type: Boolean }
+		,newCommon: { type: Boolean, default: 'true' }
+		,newParams: { type: mongoose.Schema.Types.Mixed }
+		,newTypes: { type: mongoose.Schema.Types.Mixed }
+});
+
+exports.RPUpdateClassConfig = RPUpdateClassConfig;
