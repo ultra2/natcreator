65a3de90b2b366caa72c50b16fab06b17ff6c08e
ultra2 <user10000@freemail.hu>
2013-03-17 12:51:07 -0700

    commit

---
+++ static/designer/model/RPUpdateClassConfig.js
@@ -0,0 +1,24 @@
+Ext.define('designer.model.RPUpdateClassConfig', { 
+	extend: 'designer.model.RequestPost'
+	
+	,fields: [
+		{ name: 'classId', type: 'string' }
+		,{ name: 'name', type: 'string' }
+		,{ name: 'newName', type: 'string' }
+		,{ name: 'newValues', type: 'auto' }
+		,{ name: 'newDefaultValue', type: 'auto' }
+		,{ name: 'newNotAccepted', type: 'auto' }
+		,{ name: 'newDependency', type: 'boolean' }
+		,{ name: 'newHidden', type: 'boolean' }
+		,{ name: 'newCommon', type: 'boolean', defaultValue: 'true' }
+		,{ name: 'newParams', type: 'auto' }
+		,{ name: 'newTypes', type: 'auto' }
+		,{ name: 'newReftoGetValues', type: 'auto' }
+		,{ name: 'newReftoGetValue', type: 'auto' }
+		,{ name: 'newRefType', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+});
