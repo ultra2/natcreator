bba7fd06423e256f0931b2bce5e26ac05d5a2bfb
ultra2 <user10000@freemail.hu>
2013-04-06 14:02:09 -0700

    commit

---
+++ schemas/RPCreateClassConfig.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    async = require('async'),
+    db = require('../modules/db.js');
+
+var RPCreateClassConfig = new Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,classId: { type: String }
+		,name: { type: String }
+		,types: { type: mongoose.Schema.Types.Mixed }
+		,values: { type: mongoose.Schema.Types.Mixed }
+		,defaultValue: { type: mongoose.Schema.Types.Mixed }
+		,isAccepted: { type: mongoose.Schema.Types.Mixed }
+		,dependency: { type: Boolean }
+		,hidden: { type: Boolean }
+		,common: { type: Boolean, default: 'true' }
+		,params: { type: mongoose.Schema.Types.Mixed }
+		,getValues: { type: mongoose.Schema.Types.Mixed }
+		,getValue: { type: mongoose.Schema.Types.Mixed }
+		,refType: { type: String }
+});
+
+exports.RPCreateClassConfig = RPCreateClassConfig;
