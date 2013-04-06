629147f58e7faa28d4a231a1b22473c59a880418
ultra2 <user10000@freemail.hu>
2013-04-06 14:04:41 -0700

    commit

---
+++ schemas/RPOpenProject.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    async = require('async'),
+    db = require('../modules/db.js');
+
+var RPOpenProject = new Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+		,projectId: { type: String }
+});
+
+exports.RPOpenProject = RPOpenProject;
---
+++ static/app/model/PPConfig.js
@@ -0,0 +1,11 @@
+Ext.define('natcreator.model.PPConfig', { 
+	extend: 'natcreator.model.PersistentProject'
+	
+	,fields: [
+		{ name: 'toolbox', type: 'auto' }
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+});
