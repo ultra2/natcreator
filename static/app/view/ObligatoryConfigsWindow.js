587e130622eb22d45f9169d1889f6adfcc101d5a
ultra2 <user10000@freemail.hu>
2013-04-12 22:26:06 -0700

    commit

---
+++ static/app/view/ObligatoryConfigsWindow.js
@@ -0,0 +1,12 @@
+Ext.define('natcreator.view.ObligatoryConfigsWindow', { 
+	extend: 'NAT.window.Window',
+	alias: 'widget.ObligatoryConfigsWindow',
+	
+	
+	initComponent: function(){
+		var me = this;
+		Ext.applyIf(me, {
+		});
+		me.callParent(arguments);
+	}
+});
