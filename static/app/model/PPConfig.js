10677efefa3ee042b29f3911f9c071eda65154ad
ultra2 <user10000@freemail.hu>
2013-04-21 12:41:58 -0700

    commit

---
+++ static/app/model/PPConfig.js
@@ -0,0 +1,4 @@
+Ext.define('natcreator.model.PPConfig', { 
+	extend: 'natcreator.model.PersistentProject'
+	
+});
---
+++ static/app/model/PPData.js
@@ -0,0 +1,11 @@
+Ext.define('natcreator.model.PPData', { 
+	extend: 'natcreator.model.PersistentProject'
+	
+	,fields: [
+		{ name: 'deployToHerokuStatus', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+});
