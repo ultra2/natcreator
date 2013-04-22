eb11d6f5c651a92cdf3b8efa6f4f8ddc5fe3d56d
ultra2 <user10000@freemail.hu>
2013-04-22 04:51:45 -0700

    commit

---
+++ schemas/RGSetupGithub.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    async = require('async'),
+    db = require('../modules/db.js');
+
+var RGSetupGithub = new Schema({ 
+		_id: { type: String }
+		,_type: { type: String }
+});
+
+exports.RGSetupGithub = RGSetupGithub;
---
+++ static/app/model/RPDeployFilesToHeroku.js
@@ -0,0 +1,4 @@
+Ext.define('natcreator.model.RPDeployFilesToHeroku', { 
+	extend: 'natcreator.model.RequestPost'
+	
+});
