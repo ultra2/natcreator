5088554cbb8264f818cc0a7183615ff8330f5b84
ultra2 <user10000@freemail.hu>
2013-04-26 11:54:31 -0700

    commit

---
+++ static/app/view/FileForm.js
@@ -0,0 +1,24 @@
+Ext.define('natcreator.view.FileForm', { 
+	extend: 'NAT.form.Panel',
+	alias: 'widget.FileForm',
+	
+	closable: true,
+	layout: {
+		type: 'fit'		
+	},
+	preventHeader: true,
+	
+	initComponent: function(){
+		var me = this;
+		Ext.applyIf(me, {
+			items: [
+				{
+					xtype: 'natsourceedit',					
+					itemId: 'srcContent',					
+					propertyPath: 'content'					
+				}				
+			]			
+		});
+		me.callParent(arguments);
+	}
+});
