e5e5373c9a4db89df6cb19fc27a1e92197ef886f
ultra2 <user10000@freemail.hu>
2013-05-01 04:54:37 -0700

    commit

---
+++ static/app/model/RPPollLongTask.js
@@ -0,0 +1,11 @@
+Ext.define('natcreator.model.RPPollLongTask', { 
+	extend: 'natcreator.model.RequestPost'
+	
+	,fields: [
+		{ name: 'taskName', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+});
