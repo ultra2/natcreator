90b487ad6c1684be032446b2ae62a8ad7742acc5
ultra2 <user10000@freemail.hu>
2013-05-19 12:05:28 -0700

    commit

---
+++ static/app/model/RPInstallPackage.js
@@ -0,0 +1,11 @@
+Ext.define('natcreator.model.RPInstallPackage', { 
+	extend: 'natcreator.model.RequestPost'
+	
+	,fields: [
+		{ name: 'packageId', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+	]
+});
