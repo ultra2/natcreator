ce2471f8fcc241fea787fcd41aaf9d29a9d605f6
ultra2 <user10000@freemail.hu>
2013-05-19 12:58:44 -0700

    commit

---
+++ static/app/model/QRProjectList.js
@@ -0,0 +1,16 @@
+Ext.define('natcreator.model.QRProjectList', { 
+	extend: 'NAT.data.model.Query'
+	
+	,fields: [
+		{ name: 'name', type: 'string' }
+		,{ name: 'type', type: 'string' }
+		,{ name: 'version', type: 'int' }
+		,{ name: 'roleName', type: 'string' }
+	]
+	,associations: [
+	]
+	,validations: [
+		{ field: 'name', type: 'presence' }
+		,{ field: 'roleName', type: 'presence' }
+	]
+});
---
+++ static/app/model/RequestPost.js
@@ -0,0 +1,4 @@
+Ext.define('natcreator.model.RequestPost', { 
+	extend: 'NAT.data.model.Request'
+	
+});
